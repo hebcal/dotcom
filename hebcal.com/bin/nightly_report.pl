96274616c2772b381f5086ff19da046c8fb3a8cd
Michael J. Radwin <michael@radwin.org>
2000-01-20 23:43:47 +0000

    # on 2000/01/20 23:43:47, mradwin did: Initial revision
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@158 eef4b989-ff24-4261-96ef-5f3e0244f1be

---
+++ hebcal.com/bin/nightly_report.pl
@@ -0,0 +1,192 @@
+#!/usr/local/bin/perl5 -w
+
+%known_timezones =
+    (
+     '99692', -10, # west alaska
+     '996', '??',		# west AK
+     '324', -6, # west florida
+     '325', -6, # west florida
+     '463', '??', # northwest indiana
+     '464', '??', # northwest indiana
+     '476', '??', # southwest indiana
+     '477', '??', # southwest indiana
+     '677', '??', # west kansas
+     '678', '??', # west kansas
+     '679', '??', # west kansas
+     '799', -7, # el paso
+     '798', '??', # west texas
+     '838', -8, # north idaho
+     '835', -8, # north idaho
+     '979', '??', # east oregon
+     '498', '??', # west michigan
+     '499', '??', # west michigan
+     'KS', -6,
+     'IN', -5,
+     'MI', -5,
+     'ID', -7,
+     'OR', -8,
+     'FL', -5,
+     'HI', -10,
+     'AK', -9,
+     'CA', -8,
+     'NV', -8,
+     'WA', -8,
+     'MT', -7,
+     'AZ', -7,
+     'UT', -7,
+     'WY', -7,
+     'CO', -7,
+     'NM', -7,
+     'TX', -6,
+     'OK', -6,
+     'IL', -6,
+     'WI', -6,
+     'MN', -6,
+     'IA', -6,
+     'MO', -6,
+     'AR', -6,
+     'LA', -6,
+     'MS', -6,
+     'AL', -6,
+     'OH', -5,
+     'RI', -5,
+     'MA', -5,
+     'NY', -5,
+     'NH', -5,
+     'VT', -5,
+     'ME', -5,
+     'CT', -5,
+     'NJ', -5,
+     'DE', -5,
+     'DC', -5,
+     'PA', -5,
+     'WV', -5,
+     'VA', -5,
+     'NC', -5,
+     'SC', -5,
+     'GA', -5,
+     'MD', -5,
+     'PR', -5,
+     );
+
+dbmopen(%DB,"/home/web/radwin.org/docs/hebcal/zips", 0400) || die;
+die unless defined $DB{"95051"};
+
+$yesterday = time - (60 * 60 * 24);
+
+(undef,undef,undef,$mday,$mon,$year,undef,undef,undef) =
+    localtime($yesterday);
+
+@MoY = ('Jan','Feb','Mar','Apr','May','Jun',
+	'Jul','Aug','Sep','Oct','Nov','Dec');
+
+$apache_date = sprintf("%02d/%s/%4d", $mday, $MoY[$mon], $year + 1900);
+
+$home = $faq = $holidays = $doc_other = $queries =
+    $candle = $zip = $city = $pos = 0;
+$unk_zip = $unk_tz = 0;
+%unk_zip = ();
+%unk_tz = ();
+
+while(<STDIN>)
+{
+    next unless m,/hebcal/,;
+    next unless m,\s+\[$apache_date,o;
+    next if /^207\.55\.191\.4|www\.radwin\.org|smiles\.yahoo\.com|205\.216\.162\.253|198\.144\.204\.|198\.144\.193\.150/;
+
+    $home++ if m,GET\s+/hebcal/\s+HTTP,;
+    if (m,GET\s+/michael/projects/hebcal/,)
+    {
+	if (m,GET\s+/michael/projects/hebcal/\s+HTTP,)
+	{
+	    $faq++;
+	}
+	elsif (m,GET\s+/michael/projects/hebcal/defaults.html\s+HTTP,)
+	{
+	    $holidays++;
+	}
+	else
+	{
+	    $doc_other++;
+	}
+    }
+    if (m,GET\s+/hebcal/\?, && /v=1/)
+    {
+	$queries++;
+	if (/c=(on|1)/)
+	{
+	    $candle++;
+	    $city++ if /geo=city/;
+	    $pos++ if /geo=pos/;
+	    if (/zip=(\d\d\d\d\d)/)
+	    {
+		$zipcode = $1;
+		$val = $DB{$zipcode};
+		$zip++;
+
+		if (!defined $val) {
+		    $unk_zip++;
+		    $unk_zip{$zipcode}++;
+		}
+		else
+		{
+		    (undef,$state) = split(/\0/, substr($val,6));
+		    if (/=auto/)
+		    {
+			$ok = 0;
+			if (defined $known_timezones{$zipcode})
+			{
+			    if ($known_timezones{$zipcode} ne '??')
+			    {
+				$ok = 1;
+			    }
+			}
+			elsif (defined $known_timezones{substr($zipcode,0,3)})
+			{
+			    if ($known_timezones{substr($zipcode,0,3)} ne '??')
+			    {
+				$ok = 1;
+			    }
+			}
+			elsif (defined $known_timezones{$state})
+			{
+			    if ($known_timezones{$state} ne '??')
+			    {
+				$ok = 1;
+			    }
+			}
+
+			if ($ok == 0)
+			{
+			    $unk_tz++;
+			    $unk_tz{$zipcode}++;
+			}
+		    }
+		}
+	    }
+	}
+    }
+}
+
+printf "%4d home\n", $home;
+printf "%4d faq\n", $faq;
+printf "%4d holidays\n", $holidays;
+printf "%4d doc_other\n", $doc_other;
+printf "%4d queries\n", $queries;
+printf "%4d candle (%4d zip, %4d city, %4d pos)\n",
+    $candle, $zip, $city, $pos;
+
+if ($unk_zip + $unk_tz > 0) {
+    printf "\n%4d zips unk, %4d timezone unk\n", $unk_zip, $unk_tz;
+
+    foreach (sort keys %unk_zip) {
+	printf "%s %4d\n", $_, $unk_zip{$_};
+    }
+
+    foreach (sort keys %unk_tz) {
+	($zip_city,$state) = split(/\0/, substr($DB{$_},6));
+	printf "%s, %s %s (%d pv)\n", $zip_city, $state, $_, $unk_tz{$_};
+    }
+}
+dbmclose(%DB);
+exit(0);
