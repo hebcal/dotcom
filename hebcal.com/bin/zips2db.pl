b16b49e4e2e441b729812af722e7945e66f8e1c7
Michael J. Radwin <michael@radwin.org>
2000-04-24 19:34:36 +0000

    # on 2000/04/24 19:34:36, mradwin did: foo
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@168 eef4b989-ff24-4261-96ef-5f3e0244f1be

---
+++ hebcal.com/bin/zips2db.pl
@@ -0,0 +1,62 @@
+#!/usr/local/bin/perl5 -w
+
+use DB_File;
+
+$dbmfile = 'zips.db';
+tie(%DB, 'DB_File', $dbmfile, O_RDONLY, 0444, $DB_File::DB_HASH)
+    || die "Can't tie $dbmfile: $!\n";
+
+while(<>)
+{
+    chop;
+    ($fips,$zip,$state,$city,$long,$lat,$pop,$alloc) =
+	/^"(\d+)","(\d+)","([^\"]+)","([^\"]+)",([^,]+),([^,]+),([^,]+),([^,]+)$/;
+
+    if (! defined $alloc)
+    {
+	warn "bad line $_\n";
+	next;
+    }
+
+    die if $city =~ /,/;
+
+    ($long_deg,$long_min) = split(/\./, $long, 2);
+    ($lat_deg,$lat_min) = split(/\./, $lat, 2);
+
+    if (defined $long_min && $long_min ne '')
+    {
+	$long_min = '.' . $long_min;
+    }
+    else
+    {
+	$long_min = 0;
+    }
+
+    if (defined $lat_min && $lat_min ne '')
+    {
+	$lat_min = '.' . $lat_min;
+    }
+    else
+    {
+	$lat_min = 0;
+    }
+
+    $long_min = $long_min * 60;
+    $long_min *= -1 if $long_deg < 0;
+    $long_min = sprintf("%.0f", $long_min);
+
+    $lat_min = $lat_min * 60;
+    $lat_min *= -1 if $lat_deg < 0;
+    $lat_min = sprintf("%.0f", $lat_min);
+
+    $DB{$zip} = pack('ncnc',$long_deg,$long_min,$lat_deg,$lat_min) .
+	$city . "\0" . $state;
+}
+untie(%DB);
+
+if ($^W)
+{
+    $pop = $alloc = $fips;	# touch variables to avoid warning
+}
+
+exit(0);
