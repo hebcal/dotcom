aba881e1acc3267f559bfa6fc768df214aecc96f
Michael J. Radwin <michael@radwin.org>
2000-06-13 21:24:05 +0000

    # on 2000/06/13 21:24:05, mradwin did: gnu
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@189 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/zips2db.pl
+++ hebcal.com/bin/zips2db.pl
@@ -1,5 +1,26 @@
 #!/usr/local/bin/perl5 -w
 
+########################################################################
+# zips2db.pl - generate zips.db file from Gazeteer zips.txt
+# part of the Hebcal Interactive Jewish Calendar
+#
+# Copyright (c) 2000  Michael John Radwin.  All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+########################################################################
+
 use DB_File;
 
 $dbmfile = 'zips.db';
