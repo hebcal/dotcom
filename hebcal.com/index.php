441f9e7872b6b9672354321b9a1f1fcf180bf198
Michael J. Radwin <michael@radwin.org>
2001-04-17 23:58:36 +0000

    # on 2001/04/17 23:58:36, mradwin did: Initial revision
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@354 eef4b989-ff24-4261-96ef-5f3e0244f1be

---
+++ hebcal.com/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<!-- $Id$ -->
+<html lang="en">
+<head>
+<title>Hebrew Calendar</title>
+<meta http-equiv="PICS-Label" content='(PICS-1.1 "http://www.classify.org/safesurf/" l r (SS~~000 1))'>
+<meta http-equiv="PICS-Label" content='(PICS-1.1 "http://www.rsac.org/ratingsv01.html" l gen true on "1998.03.10T11:49-0800" r (n 0 s 0 v 0 l 0))'>
+<meta name="author" content="Michael John Radwin">
+<link rev="made" href="mailto:webmaster@hebcal.com">
+<base href="http://www.hebcal.com/" target="_top">
+<style type="text/css">
+<!--
+.navbar {background: #ffcc99 ; color: #000000 ; border-style: none none solid none; border-color: #ff9966 ; border-width: thin; padding: 2px;}
+H1, H2, H3, H4, H5, H6 {
+   font-family : Tahoma, Verdana, Arial, Helvetica, Geneva, SunSans-Regular, sans-serif;
+   margin-bottom : 0;
+}
+-->
+</style>
+</head>
+<body>
+<form action="/cgi-bin/htsearch" method="get">
+<table width="100%" class="navbar">
+<tr><td><small><strong>hebcal.com</strong></small></td>
+<td align="right"><input type="text" name="words" size="30">
+<input type="submit" value="Search"></td></tr></table>
+</form>
+<h1>Hebrew Calendar</h1>
+<p>hebcal.com is a free site that provides web tools for the Jewish
+calendar.</p>
+<dl>
+<dt><strong><big><a
+href="/hebcal/">Hebcal Interactive Jewish
+Calendar</a></big></strong></dt>
+<dd>generates a list of Jewish holidays and candle lighting times customized
+to your zip code, city, or latitude/longitude.</dd>
+<dt><br><strong><big><a
+href="/shabbat/">1-Click Shabbat</a></big></strong></dt>
+<dd>bookmark this page for weekly Shabbat candle lighting times.</dd>
+<dt><br><strong><big><a
+href="/yahrzeit/">Yahrzeit Calendar</a></big></strong></dt>
+<dd>generates a list of Yahrzeit (memorial) and Yizkor dates.</dd>
+<dt><br><strong><big><a
+href="/search/">Search</a></big></strong></dt>
+<dd>Search the whole <code>hebcal.com</code> website.</dd>
+</dl>
+<hr noshade="noshade" size="1">
+<small>
+<!-- hhmts start -->
+Last modified: Tue Apr 17 16:58:07 PDT 2001
+<!-- hhmts end -->
+</small>
+</body>
+</html>
+
