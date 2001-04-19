601d9fe790bd68388384f1fc3d1e246fe3c1f9b9
Michael J. Radwin <michael@radwin.org>
2001-04-19 23:22:57 +0000

    # on 2001/04/19 23:22:57, mradwin did: Danny's copyright
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@405 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/index.php
+++ hebcal.com/index.php
@@ -57,8 +57,13 @@ href="/search/">Search</a></big></strong></dt>
 <a href="/privacy/">Privacy Policy</a> -
 <a href="/help/">Help</a>
 <br>
+This website uses <a href="http://www.sadinoff.com/hebcal/">hebcal 3.2
+for UNIX</a>, Copyright &copy; 1994 <a
+href="http://www.sadinoff.com/">Danny Sadinoff</a>.  All rights
+reserved.
+<br>
 <!-- hhmts start -->
-Last modified: Wed Apr 18 11:15:54 PDT 2001
+Last modified: Thu Apr 19 16:21:59 PDT 2001
 <!-- hhmts end -->
 </font>
 </body></html>
