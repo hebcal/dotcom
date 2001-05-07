0cc719d07ec685cb90fba09a697026637d490ab4
Michael J. Radwin <michael@radwin.org>
2001-05-07 21:05:44 +0000

    # on 2001/05/07 21:05:44, mradwin did: generate the default holiday info page from holidays.ini
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@469 eef4b989-ff24-4261-96ef-5f3e0244f1be

---
+++ hebcal.com/bin/gen_holidays.pl
@@ -0,0 +1,143 @@
+#!/usr/local/bin/perl5 -w
+
+# $Id$
+
+use Hebcal;
+use Getopt::Std;
+use Config::IniFiles;
+use strict;
+
+$0 =~ s,.*/,,;  # basename
+
+my($usage) = "usage: $0 [-h] holidays.ini holidays.html
+    -h        Display usage information.
+";
+
+my(%opts);
+&getopts('h', \%opts) || die "$usage\n";
+$opts{'h'} && die "$usage\n";
+(@ARGV == 2) || die "$usage";
+
+my($this_year) = (localtime)[5];
+$this_year += 1900;
+
+my($rcsrev) = '$Revision$'; #'
+$rcsrev =~ s/\s*\$//g;
+
+my($infile) = shift;
+my($outfile) = shift;
+
+my($holidays) = new Config::IniFiles(-file => $infile);
+$holidays || die "$infile: $!\n";
+
+my(%out);
+foreach my $h ($holidays->Sections())
+{
+    next unless defined $holidays->val($h, 'ord') &&
+	defined $holidays->val($h, 'class');
+    $out{$holidays->val($h, 'class')}->{$holidays->val($h, 'ord')} = $h;
+}
+
+open(OUT, ">$outfile") || die "$outfile: $!\n";
+
+print OUT <<EOHTML;
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html><head><title>Hebcal Default Holidays</title>
+<meta http-equiv="PICS-Label" content='(PICS-1.1 "http://www.rsac.org/ratingsv01.html" l gen true for "http://www.hebcal.com" r (n 0 s 0 v 0 l 0))'>
+<base href="http://www.hebcal.com/help/defaults.html" target="_top">
+<link rev="made" href="mailto:webmaster\@hebcal.com">
+<link rel="stylesheet" href="/style.css" type="text/css">
+<link rel="p3pv1" href="http://www.hebcal.com/w3c/p3p.xml">
+</head>
+<body>
+<table width="100%" class="navbar">
+<tr><td><small>
+<strong><a href="/">hebcal.com</a></strong> <tt>-&gt;</tt>
+<a href="/help/">Help</a> <tt>-&gt;</tt>
+Default Holidays
+</small></td>
+<td align="right"><small><a
+href="/search/">Search</a></small>
+</td></tr></table>
+<h1>Hebcal Default Holidays</h1>
+EOHTML
+;
+
+print OUT qq{<h2>Major Holidays</h2>
+<p>For a good reference book on the major Jewish Holidays, I'd suggest
+Michael Strassfeld's <em><a
+href="http://www.amazon.com/exec/obidos/ISBN=0062720082/hebrewcalendaror/">The
+Jewish Holidays: A Guide & Commentary</a></em>.</p>
+};
+&do_section('major');
+
+print OUT "<h3>Special Shabbatot</h3>\n";
+&do_section('shabbat');
+
+print OUT "<h3>Minor Fast Days</h3>\n";
+&do_section('fast');
+
+print OUT "<h3>New Holidays</h3>\n";
+&do_section('modern');
+
+print OUT "<h2>Rosh Chodesh</h2>\n";
+&do_section('rc');
+
+print OUT <<EOHTML;
+<h2>Other Hebcal Holidays</h2>
+<dl>
+<dt><a name="omer"
+href="http://www.jewfaq.org/holidayb.htm">Days of the Omer</a>
+<dd>7 weeks from the second night of Pesach to the day before Shavuot
+</dl>
+<p>
+<hr noshade size="1">
+<font size=-2 face=Arial>Copyright
+&copy; $this_year Michael J. Radwin. All rights reserved.
+<a href="/privacy/">Privacy Policy</a> -
+<a href="/help/">Help</a>
+<br>
+<!-- hhmts start -->
+Last modified: Fri Apr 27 10:35:30 PDT 2001
+<!-- hhmts end -->
+($rcsrev)
+</font>
+</body></html>
+EOHTML
+;
+
+close(OUT);
+exit(0);
+
+sub do_section
+{
+    my($class) = @_;
+
+    print OUT "<dl>\n";
+    foreach my $ord (sort {$a <=> $b} keys %{$out{$class}})
+    {
+	my($h) = $out{$class}->{$ord};
+	my($anchor) = $holidays->val($h, 'anchor');
+	my($href) = $holidays->val($h, 'href');
+	my($descr) = $holidays->val($h, 'descr');
+
+	if (defined $href) {
+	    $href = "\nhref=\"$href\"";
+	} else {
+	    $href = '';
+	}
+
+	if (defined $descr) {
+	    $descr = "<dd>$descr\n";
+	} else {
+	    $descr = '';
+	}
+
+	$h =~ s/\s+I$//;	# Pesach I, Sukkot I, etc.
+	$h =~ s/:\s+\d+.+$//;	# Chanukah: 1 Candle
+
+	print OUT qq{<dt><a name="$anchor"$href>$h</a>\n$descr};
+    }
+    print OUT "</dl>\n";
+}
