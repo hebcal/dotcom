28fa6033189ecf397fd1b54c62dbd3a3bb26f301
Michael J. Radwin <michael@radwin.org>
2001-07-06 23:53:53 +0000

    # on 2001/07/06 23:53:53, mradwin did: create a subdirectory, one page per parashat
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@579 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/gen_sedrot.pl
+++ hebcal.com/bin/gen_sedrot.pl
@@ -9,7 +9,7 @@ use strict;
 
 $0 =~ s,.*/,,;  # basename
 
-my($usage) = "usage: $0 [-h] sedrot.ini sedrot.html
+my($usage) = "usage: $0 [-h] sedrot.ini output-dir
     -h        Display usage information.
 ";
 
@@ -25,19 +25,22 @@ my($rcsrev) = '$Revision$'; #'
 $rcsrev =~ s/\s*\$//g;
 
 my($infile) = shift;
-my($outfile) = shift;
+my($outdir) = shift;
 
 my($sedrot) = new Config::IniFiles(-file => $infile);
 $sedrot || die "$infile: $!\n";
 
-open(OUT, ">$outfile") || die "$outfile: $!\n";
+my($mtime) = (stat($infile))[9];
+my($hhmts) = "Last modified:\n" . localtime($mtime);
+
+open(OUT, ">$outdir/index.html") || die "$outdir/index.html: $!\n";
 
 print OUT <<EOHTML;
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 	"http://www.w3.org/TR/html4/loose.dtd">
 <html><head><title>Hebcal: Torah Readings</title>
 <meta http-equiv="PICS-Label" content='(PICS-1.1 "http://www.rsac.org/ratingsv01.html" l gen true for "http://www.hebcal.com" r (n 0 s 0 v 0 l 0))'>
-<base href="http://www.hebcal.com/help/sedrot.html" target="_top">
+<base href="http://www.hebcal.com/sedrot/" target="_top">
 <link rev="made" href="mailto:webmaster\@hebcal.com">
 <link rel="stylesheet" href="/style.css" type="text/css">
 <link rel="p3pv1" href="http://www.hebcal.com/w3c/p3p.xml">
@@ -47,7 +50,6 @@ print OUT <<EOHTML;
 <table width="100%" class="navbar">
 <tr><td><small>
 <strong><a href="/">hebcal.com</a></strong> <tt>-&gt;</tt>
-<a href="/help/">Help</a> <tt>-&gt;</tt>
 Torah Readings
 </small></td>
 <td align="right"><small><a
@@ -55,9 +57,7 @@ href="/search/">Search</a></small>
 </td></tr></table>
 <h1>Torah Readings</h1>
 
-<p>
-<table border="1" cellpadding="4">
-<tr><th>Parashat</th><th>Torah + Aliyot</th><th>Haftarah</th></tr>
+<dl>
 EOHTML
 ;
 
@@ -66,43 +66,17 @@ foreach my $h ($sedrot->Sections())
 {
     next if $h =~ /^Combined /;
 
-    my($href,$hebrew,$memo,$torah_href,$haftarah_href) =
-	&Hebcal::get_holiday_anchor("Parashat $h", 0);
-    my(undef,undef,$memo2) =
-	&Hebcal::get_holiday_anchor("Parashat $h", 1);
-
-    $memo =~ /Torah: (.+) \/ Haftarah: (.+)$/;
-    my($torah,$haftarah) = ($1,$2);
-
-    $memo2 =~ /Torah: .+ \/ Haftarah: (.+)$/;
-    my($haftarah_seph) = $1;
-
-    my($seph) = ($haftarah_seph eq $haftarah) ? '' : "<br>($haftarah_seph)";
-
     my($anchor) = $h;
     $anchor = lc($anchor);
     $anchor =~ s/[^\w]//g;
 
-    print OUT qq{<tr><td><big><a name="$anchor">$h</a><br>\n},
-    qq{<span dir="rtl" class="hebrew"\n},
-    qq{lang="he">$hebrew</span></big></td>\n},
-    qq{<td><a href="$torah_href">$torah</a>\n},
-    qq{<small><ol>};
+    print OUT qq{<dt><a name="$anchor" href="$anchor.html">Parashat\n$h</a>\n};
 
-    foreach (1 .. 7)
-    {
-	print OUT qq{<li>}, $sedrot->val($h, "aliyah$_"), "\n";
-    }
-
-    print OUT qq{</ol></small></td>\n},
-    qq{<td><a href="$haftarah_href">$haftarah</a>$seph</td></tr>\n};
+    &write_sedra_page($h);
 }
 
-my($mtime) = (stat($infile))[9];
-my($hhmts) = "Last modified:\n" . localtime($mtime);
-
 print OUT <<EOHTML;
-</table>
+</dl>
 <p>
 <hr noshade size="1">
 <font size=-2 face=Arial>Copyright
@@ -119,3 +93,89 @@ EOHTML
 
 close(OUT);
 exit(0);
+
+sub write_sedra_page {
+    my($h) = @_;
+
+    my($drash_href,$hebrew,$memo,$torah_href,$haftarah_href) =
+	&Hebcal::get_holiday_anchor("Parashat $h", 0);
+    my(undef,undef,$memo2) =
+	&Hebcal::get_holiday_anchor("Parashat $h", 1);
+
+    $memo =~ /Torah: (.+) \/ Haftarah: (.+)$/;
+    my($torah,$haftarah) = ($1,$2);
+
+    $memo2 =~ /Torah: .+ \/ Haftarah: (.+)$/;
+    my($haftarah_seph) = $1;
+
+    my($seph) = ($haftarah_seph eq $haftarah) ? '' :
+	"<br>Haftarah for Sephardim: $haftarah_seph";
+
+    my($anchor) = $h;
+    $anchor = lc($anchor);
+    $anchor =~ s/[^\w]//g;
+
+    open(OUT2, ">$outdir/$anchor.html") || die "$outdir/$anchor.html: $!\n";
+
+    print OUT2 <<EOHTML;
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html><head><title>Torah Readings: $h</title>
+<meta http-equiv="PICS-Label" content='(PICS-1.1 "http://www.rsac.org/ratingsv01.html" l gen true for "http://www.hebcal.com" r (n 0 s 0 v 0 l 0))'>
+<base href="http://www.hebcal.com/sedrot/$anchor.html" target="_top">
+<link rev="made" href="mailto:webmaster\@hebcal.com">
+<link rel="stylesheet" href="/style.css" type="text/css">
+<link rel="p3pv1" href="http://www.hebcal.com/w3c/p3p.xml">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<table width="100%" class="navbar">
+<tr><td><small>
+<strong><a href="/">hebcal.com</a></strong> <tt>-&gt;</tt>
+<a href="/sedrot/">Torah Readings</a> <tt>-&gt;</tt>
+$h
+</small></td>
+<td align="right"><small><a
+href="/search/">Search</a></small>
+</td></tr></table>
+
+<br>
+<table width="100%">
+<tr>
+<td><h1>Parashat $h</h1></td>
+<td><h1 dir="rtl" class="hebrew" name="hebrew"
+lang="he">$hebrew</h1></td>
+</tr>
+</table>
+<h3><a name="torah">Torah Portion:</a>
+<a href="$torah_href">$torah</a></h3>
+Shabbat aliyot (full kriyah):
+<ol>
+EOHTML
+;
+    foreach (1 .. 7)
+    {
+	print OUT2 qq{<li>}, $sedrot->val($h, "aliyah$_"), "\n";
+    }
+
+    print OUT2 <<EOHTML;
+</ol>
+<h3><a name="haftarah">Haftarah:</a>
+<a href="$haftarah_href">$haftarah</a>$seph</h3>
+<h3><a name="drash" href="$drash_href">Commentary</a></h3>
+
+<p>
+<hr noshade size="1">
+<font size=-2 face=Arial>Copyright
+&copy; $this_year Michael J. Radwin. All rights reserved.
+<a href="/privacy/">Privacy Policy</a> -
+<a href="/help/">Help</a>
+<br>
+$hhmts
+($rcsrev)
+</font>
+</body></html>
+EOHTML
+;
+
+}
