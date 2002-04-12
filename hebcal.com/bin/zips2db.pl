fb135dbdcde63ff5586d06ab11b232a372d4716f
Michael J. Radwin <michael@radwin.org>
2002-04-12 22:08:42 +0000

    # on 2002/04/12 22:08:42, mradwin did: new 1999 format using National Weather Service county-timezone data
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@896 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/zips2db.pl
+++ hebcal.com/bin/zips2db.pl
@@ -28,76 +28,143 @@ use strict;
 
 $0 =~ s,.*/,,;  # basename
 
-my($usage) = "usage: $0 [-h] zips.txt
+my($usage) = "usage: $0 [-h] output.db zipnov99.csv c_22mr02.csv
     -h        Display usage information.
 ";
 
 my(%opts);
 &getopts('h', \%opts) || die "$usage\n";
 $opts{'h'} && die "$usage\n";
-(@ARGV == 1) || die "$usage";
+(@ARGV == 3) || die "$usage";
 
-open(IN, $ARGV[0]) || die "$ARGV[0]: $!\n";
+## National Weather Service county-timezone data comes from a file named
+## c_DDmmYY.zip from http://isl715.nws.noaa.gov/mapdata/newcat/amdc/county/
+##
+## In this data, timezone is a single letter like, P, M, C, E or p, m,
+## c, e.  Uppercase indicates the county observes DST, lowercase
+## indicates it doesn't.  Puerto Rico, Guam and Pago Pago time zones are
+## all expressed in caps, but do NOT observe DST.
 
-my $dbmfile = 'zips.db';
+my(%tz_abbrev) =
+    (
+     'E' => '-5,1',    # New York
+     'e' => '-5,0',    # Indianapolis
+     'C' => '-6,1',    # Chicago
+     'c' => '-6,0',    # Regina
+     'M' => '-7,1',    # Denver
+     'm' => '-7,0',    # Phoenix
+     'P' => '-8,1',    # Los Angeles
+     'p' => undef,     # (doesn't appear)
+     'A' => '-9,1',    # Anchorage
+     'a' => undef,     # (doesn't appear)
+     'h' => undef,     # (doesn't appear)
+     'H' => '-10,0',   # Honolulu
+     'AH' => '-10,1',  # Aleutians West, AK
+     'V' => '-4,0',    # Puerto Rico
+     'G' => '10,0',    # Guam
+     'S' => '-11,0',   # Pago Pago
+     'CE' => '?,?',    # for unknown
+     'CM' => '?,?',    # for unknown
+     'MC' => '?,?',    # for unknown
+     'MP' => '?,?',    # for unknown
+     '?' => '?,?',     # for unknown
+     );
+
+my($dbmfile,$zips_file,$weather_file) = @ARGV;
+
+my(%fips_zone,%fips_state);
+
+open(IN,$weather_file) || die "$weather_file: $!\n";
+print "reading timezone data from weather file $weather_file\n";
+my $count = 0;
+$_ = <IN>;			# ignore header
+while (<IN>)
+{
+    chop;
+    s/\cM$//;			# nuke DOS line-ending
+
+    my($state,$cwa,$countyname,$fips,$time_zone,$fe_area,$lon,$lat) =
+	split(/,/);
+
+    unless (defined $time_zone)
+    {
+	warn "$weather_file:$.: bad format\n";
+	next;
+    }
+
+    $fips_zone{$fips} = $time_zone;
+    $fips_state{$fips} = $state;
+    $count++;
+}
+close(IN);
+print "read $count counties from $weather_file\n";
+
+open(IN,$zips_file) || die "$zips_file: $!\n";
+
 my %DB;
 tie(%DB, 'DB_File', $dbmfile, O_RDWR|O_CREAT, 0644, $DB_File::DB_HASH)
     || die "Can't tie $dbmfile: $!\n";
 
-print "reading from $ARGV[0], writing to zips.db\n";
+print "reading zipcodes from $zips_file, writing to $dbmfile\n";
 
-my $count = 0;
+$count = 0;
+my $matched = 0;
+my(%seen);
+
+$_ = <IN>;			# ignore header
 while(<IN>)
 {
     chop;
-    my($fips,$zip,$state,$city,$long,$lat,$pop,$alloc) =
-	/^"(\d+)","(\d+)","([^\"]+)","([^\"]+)",([^,]+),([^,]+),([^,]+),([^,]+)$/;
+    s/\cM$//;			# nuke DOS line-ending
 
-    if (! defined $alloc)
+    my($zip_code,$latitude,$longitude,$zip_class,$poname,$state,$county) =
+	split(/,/);
+
+    unless (defined $county)
     {
-	warn "bad line $_\n";
+	warn "$zips_file:$.: bad format\n";
 	next;
     }
 
-    die if $city =~ /,/;
+    my($fips) = $state . $county;
+    my($tz,$us_state);
 
-    my($long_deg,$long_min) = split(/\./, $long, 2);
-    my($lat_deg,$lat_min) = split(/\./, $lat, 2);
-
-    if (defined $long_min && $long_min ne '')
+    if (defined $fips_zone{$fips})
     {
-	$long_min = '.' . $long_min;
+	$tz = $fips_zone{$fips};
+	$us_state = $fips_state{$fips};
+	$matched++;
     }
     else
     {
-	$long_min = 0;
+	warn "$zips_file:$.: unknown timezone for FIPS $fips\n";
+	$tz = '?';
+	$us_state = '??';
     }
 
-    if (defined $lat_min && $lat_min ne '')
+    if (defined $seen{$zip_code})
     {
-	$lat_min = '.' . $lat_min;
+	warn "$zips_file:$.: duplicate zipcode $zip_code (already seen on line $seen{$zip_code})\n";
+	next;
     }
     else
     {
-	$lat_min = 0;
+	$seen{$zip_code} = $.;
     }
 
-    $long_min = $long_min * 60;
-    $long_min *= -1 if $long_deg < 0;
-    $long_min = sprintf("%.0f", $long_min);
+    unless (defined $tz_abbrev{$tz})
+    {
+	die "no tz_abbrev for $tz";
+    }
 
-    $lat_min = $lat_min * 60;
-    $lat_min *= -1 if $lat_deg < 0;
-    $lat_min = sprintf("%.0f", $lat_min);
-
-    warn "$zip exists, replacing" if defined $DB{$zip};
-    $DB{$zip} = pack('ncnc',$long_deg,$long_min,$lat_deg,$lat_min) .
-	$city . "\0" . $state;
+    $DB{$zip_code} = join(',', $latitude,$longitude,
+			  $tz_abbrev{$tz},$poname,$us_state);
     $count++;
 }
 close(IN);
 untie(%DB);
 
-print "inserted $count zips from $ARGV[0] into zips.db\n";
+print "inserted $count zipcodes from $zips_file into $dbmfile\n";
+print "$matched / $count zipcodes had a timezone\n";
 
 exit(0);
