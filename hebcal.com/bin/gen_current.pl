b024cb29ac32ba5c5ce448912758efc91dc954f2
Michael J. Radwin <michael@radwin.org>
2002-07-08 22:40:11 +0000

    # on 2002/07/08 22:40:11, mradwin did: Initial revision
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@970 eef4b989-ff24-4261-96ef-5f3e0244f1be

---
+++ hebcal.com/bin/gen_current.pl
@@ -0,0 +1,29 @@
+#!/usr/local/bin/perl -w
+
+use Hebcal;
+use strict;
+
+my $line = `./bin/hebcal -S -t -x -h | grep Parashat`;
+chomp($line);
+
+if ($line =~ m,^\d+/\d+/\d+\s+(.+)\s*$,) {
+    my $parsha = $1;
+    my $href = &Hebcal::get_holiday_anchor($parsha);
+    if ($href) {
+	my($now) = time();
+	my($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
+	    localtime($now);
+	my($saturday) = ($wday == 6) ?
+	    $now + (60 * 60 * 24) : $now + ((6 - $wday) * 60 * 60 * 24);
+	my($stime) = strftime("%d %B %Y", localtime($saturday));
+
+	open(OUT,">current2.inc") || die;
+	print OUT <<EOHTML;
+<br><br><span class="sm-grey">&gt;</span>
+<b><a href="$href">$parsha</a></b>
+<br>$stime
+EOHTML
+    ;
+    }
+}
+
