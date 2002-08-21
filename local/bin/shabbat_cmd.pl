962436374e751fb60f24acdc9120f52f8d0cef15
Michael J. Radwin <michael@radwin.org>
2002-08-21 03:28:41 +0000

    # on 2002/08/21 03:28:41, mradwin did: use Hebcal::emaildb_lock() routines to share lock with web
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1030 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- local/bin/shabbat_cmd.pl
+++ local/bin/shabbat_cmd.pl
@@ -5,6 +5,7 @@ use lib "/pub/m/r/mradwin/private/lib/perl5/site_perl";
 use strict;
 use DB_File;
 use Fcntl qw(:DEFAULT :flock);
+use Hebcal;
 
 my($op,$addr) = @ARGV;
 if (defined $op && $op =~ /^unsub/i && $addr) {
@@ -19,33 +20,28 @@ sub unsubscribe
 {
     my($email) = @_;
 
+    my $lockfd = &Hebcal::emaildb_lock(LOCK_EX);
+
     my $dbmfile = '/pub/m/r/mradwin/hebcal.com/email/subs.db';
     my(%DB);
     my($db) = tie(%DB, 'DB_File', $dbmfile, O_CREAT|O_RDWR, 0644,
 		  $DB_File::DB_HASH)
 	or die "$dbmfile: $!\n";
 
-    my($fd) = $db->fd;
-    open(DB_FH, "+<&=$fd") || die "dup $!";
-
-    unless (flock (DB_FH, LOCK_EX)) { die "flock: $!" }
-
     my $args = $DB{$email};
     unless ($args) {
 	warn "ignoring $email: not subscribed\n";
-	flock(DB_FH, LOCK_UN);
 	undef $db;
 	untie(%DB);
-	close(DB_FH);
+	&Hebcal::emaildb_unlock($lockfd);
 	return 0;
     }
 
     if ($args =~ /^action=/) {
 	warn "ignoring $email: already unsubscribed\n";
-	flock(DB_FH, LOCK_UN);
 	undef $db;
 	untie(%DB);
-	close(DB_FH);
+	&Hebcal::emaildb_unlock($lockfd);
 	return 0;
     }
 
@@ -53,10 +49,10 @@ sub unsubscribe
     $DB{$email} = "action=UNSUBSCRIBE;t=$now";
 
     $db->sync;
-    flock(DB_FH, LOCK_UN);
     undef $db;
     untie(%DB);
-    close(DB_FH);
+
+    &Hebcal::emaildb_unlock($lockfd);
 
     warn "unsubscribe $email: OK\n";
 
