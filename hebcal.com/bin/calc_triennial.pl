222ccb0060190f1e6548c77c53db137fdb914b20
Michael J. Radwin <michael@radwin.org>
2002-08-21 04:08:02 +0000

    # on 2002/08/21 04:08:02, mradwin did: add GPL
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1033 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/calc_triennial.pl
+++ hebcal.com/bin/calc_triennial.pl
@@ -1,6 +1,35 @@
-#!/usr/local/bin/perl -w -I/pub/p/e/perl/lib/site_perl:/home/mradwin/local/lib/perl5:/home/mradwin/local/lib/perl5/site_perl
+#!/usr/local/bin/perl -w
 
+########################################################################
+# Generates the Torah Readings for http://www.hebcal.com/sedrot/
+#
+# Calculates full kriyah according to standard tikkun
+#
+# Calculates triennial according to
+#   A Complete Triennial System for Reading the Torah
+#   http://learn.jtsa.edu/topics/diduknow/responsa/trichart.shtml
+#
 # $Id$
+#
+# Copyright (c) 2002  Michael J. Radwin.  All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+########################################################################
+
+use lib "/pub/m/r/mradwin/private/lib/perl5/site_perl";
+use lib "/pub/p/e/perl/lib/site_perl";
 
 use Hebcal;
 use Getopt::Std;
