c83f34c98cf7ad80dd39cea53ec8454b1572b531
Michael J. Radwin <michael@radwin.org>
2003-05-15 19:50:53 +0000

    # on 2003/05/15 19:50:53, mradwin did: prune
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1300 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- local/bin/shabbat_cmd.pl
+++ local/bin/shabbat_cmd.pl
@@ -8,15 +8,48 @@ use Hebcal;
 
 sub main() {
     my($op,$addr) = @ARGV;
+
+    my $usage = "usage: $0 {unsub|bounce} email-addr\nusage: $0 prune";
+
     if (!defined $op) {
-	die "usage: $0 {unsub|bounce} email-addr\n";
+	die "$usage\n";
+    } elsif ($op eq 'prune') {
+	prune();
     } elsif ($op =~ /^unsub/i && $addr) {
 	unsubscribe(lc($addr), 'UNSUBSCRIBE');
     } elsif ($op =~ /^bounce/i && $addr) {
 	unsubscribe(lc($addr), 'BOUNCE');
     } else {
-	die "usage: $0 {unsub|bounce} email-addr\n";
+	die "$usage\n";
     }
+}
+
+sub prune() {
+    my $dbmfile = '/pub/m/r/mradwin/hebcal.com/email/email.db';
+    my(%DB);
+    tie(%DB, 'DB_File::Lock', $dbmfile, O_CREAT|O_RDWR, 0644, $DB_HASH, 'write')
+	or die "$dbmfile: $!\n";
+
+    my @ids;
+    my $prune_time = time - (90 * 24 * 60 * 60);
+
+    while (my($rand,$config) = each(%DB)) {
+	my %args;
+	foreach my $kv (split(/;/, $config)) {
+	    my($key,$val) = split(/=/, $kv, 2);
+	    $args{$key} = $val;
+	}
+	if (defined $args{'t'} && $args{'t'} < $prune_time) {
+	    push(@ids, $rand);
+	}
+    }
+
+    foreach my $id (@ids) {
+	print "$DB{$id}\n";
+	delete $DB{$id};
+    }
+
+    untie(%DB);
 }
 
 sub unsubscribe($$) {
