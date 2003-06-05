7d7f55dac519d0aa86fa284cf5ffc2e4b6305125
Michael J. Radwin <michael@radwin.org>
2003-06-05 17:59:59 +0000

    # on 2003/06/05 17:59:59, mradwin did: truncate parsha include if needed
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1312 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/gen_current.pl
+++ hebcal.com/bin/gen_current.pl
@@ -5,9 +5,10 @@ use Hebcal;
 use POSIX;
 use strict;
 
+my $outfile = "/pub/m/r/mradwin/hebcal.com/current.inc";
 my $line = `/pub/m/r/mradwin/hebcal.com/bin/hebcal -S -t -x -h | grep Parashat`;
 chomp($line);
-
+my $wrote_parsha = 0;
 if ($line =~ m,^\d+/\d+/\d+\s+(.+)\s*$,) {
     my $parsha = $1;
     my $href = &Hebcal::get_holiday_anchor($parsha);
@@ -19,37 +20,44 @@ if ($line =~ m,^\d+/\d+/\d+\s+(.+)\s*$,) {
 	    $now + (60 * 60 * 24) : $now + ((6 - $wday) * 60 * 60 * 24);
 	my($stime) = strftime("%d %B %Y", localtime($saturday));
 
-	open(OUT,">/pub/m/r/mradwin/hebcal.com/current.inc") || die;
+	open(OUT,">$outfile") || die;
 	$parsha =~ s/ /&nbsp;/g;
 	print OUT "<br><br><span class=\"sm-grey\">&gt;</span>&nbsp;<b><a\n";
 	print OUT "href=\"$href\">$parsha</a></b><br>$stime";
 	close(OUT);
+	$wrote_parsha = 1;
     }
 }
+unless ($wrote_parsha) {
+    open(OUT,">$outfile") || die;
+    close(OUT);
+}
 
-my $inc = '/pub/m/r/mradwin/hebcal.com/today.inc';
-open(OUT,">$inc") || die;
+$outfile = '/pub/m/r/mradwin/hebcal.com/today.inc';
+open(OUT,">$outfile") || die;
 print OUT `/pub/m/r/mradwin/hebcal.com/bin/hebcal -T -x -h | grep -v Omer`;
 close(OUT);
 
-$inc = '/pub/m/r/mradwin/hebcal.com/holiday.inc';
-open(OUT,">$inc") || die;
-close(OUT);
-
+$outfile = '/pub/m/r/mradwin/hebcal.com/holiday.inc';
 $line = `/pub/m/r/mradwin/hebcal.com/bin/hebcal -t | grep -v ' of '`;
 chomp($line);
-
+my $wrote_holiday = 0;
 if ($line =~ m,^\d+/\d+/\d+\s+(.+)\s*$,) {
     my $holiday = $1;
     my $href = &Hebcal::get_holiday_anchor($holiday);
     if ($href) {
 	my($stime) = strftime("%d %B %Y", localtime(time()));
-	open(OUT,">$inc") || die;
+	open(OUT,">$outfile") || die;
 	$holiday =~ s/ /&nbsp;/g;
 	print OUT "<br><br><span class=\"sm-grey\">&gt;</span>&nbsp;<b><a\n";
 	print OUT "href=\"$href\">$holiday</a></b><br>$stime\n";
 	close(OUT);
+	$wrote_holiday = 1;
     }
+}
+unless ($wrote_holiday) {
+    open(OUT,">$outfile") || die;
+    close(OUT);
 }
 
 
