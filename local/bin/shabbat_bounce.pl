649e4474da748cfca8bc3b03f73dc55e20f2e4e4
Michael J. Radwin <michael@radwin.org>
2003-10-08 19:25:58 +0000

    # on 2003/10/08 19:25:58, mradwin did: working correctly
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1398 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- local/bin/shabbat_bounce.pl
+++ local/bin/shabbat_bounce.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl -w
+#!/usr/local/bin/perl
 
 # $Source: /Users/mradwin/hebcal-copy/local/bin/RCS/shabbat_bounce.pl,v $
 # $Id$
@@ -14,51 +14,53 @@ use Mail::DeliveryStatus::BounceParser;
 my $site = 'hebcal.com';
 my $dsn = 'DBI:mysql:database=hebcal1;host=mysql.hebcal.com';
 
-my $bounce = eval { Mail::DeliveryStatus::BounceParser->new(\*STDIN) };
+my $message = new Mail::Internet \*STDIN;
+my $header = $message->head();
+my $to = $header->get('To');
+
+my($email_address,$bounce_reason);
+if ($to) {
+    chomp($to);
+    if ($to =~ /^[^<]*<([^>]+)>/) {
+	$to = $1;
+    }
+    if (Email::Valid->address($to)) {
+	$to = Email::Valid->address($to);
+    } else {
+	warn $Email::Valid::Details;
+    }
+
+    if ($to =~ /shabbat-return-([^\@]+)\@/) {
+	$email_address = $1;
+	$email_address =~ s/=/\@/;
+    }
+}
+
+my $bounce = eval { Mail::DeliveryStatus::BounceParser->new($message->as_string()) };
 if ($@) { 
     # couldn't parse.  ignore this message.
-    exit(0);
-}
-
-my @reports = $bounce->reports;
-foreach my $report (@reports) {
-    my $to = $report->get('To');
-    if ($to) {
-	chomp($to);
-	if ($to =~ /^[^<]*<([^>]+)>/) {
-	    $to = $1;
-	}
-	if (Email::Valid->address($to)) {
-	    $to = Email::Valid->address($to);
-	} else {
-	    warn $Email::Valid::Details;
-	}
-
-	if ($to =~ /shabbat-return-([^\@]+)\@/) {
-	    shabbat_bounce($report,$1);
-	}
+    $bounce_reason = 'unknown (unable to parse message)';
+} else {
+    my @reports = $bounce->reports;
+    foreach my $report (@reports) {
+	my $reason = $report->get('reason');
+	$bounce_reason = defined($reason) ? $reason : $report->get('std_reason');
     }
 }
 
-sub shabbat_bounce
-{
-    my($report,$email) = @_;
-    $email =~ s/=/\@/;
+$email_address =~ s/\'/\\\'/g;
+$bounce_reason =~ s/\'/\\\'/g;
 
-    my $reason = $report->get('reason');
-    if (! defined($reason)) {
-	$reason = $report->get('std_reason');
-    }
+my $dbh = DBI->connect($dsn, 'mradwin_hebcal', 'xxxxxxxx');
 
-    my $dbh = DBI->connect($dsn, 'mradwin_hebcal', 'xxxxxxxx');
-
-    my $sql = <<EOD
+my $sql = <<EOD
 INSERT INTO hebcal1.hebcal_shabbat_bounce
        (bounce_time, bounce_address, bounce_reason)
-VALUES (NOW(),       '$email',       '$reason')
+VALUES (NOW(), '$email_address', '$bounce_reason')
 EOD
 ;
-    $dbh->do($sql);
-    $dbh->disconnect;
-}
+
+$dbh->do($sql);
+$dbh->disconnect;
+exit(0);
 
