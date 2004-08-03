68f3b28649871a59802705b10883f090cd4ee583
Michael J. Radwin <michael@radwin.org>
2004-08-03 22:40:05 +0000

    # on 2004/08/03 22:40:05, mradwin did: label for aunz DST scheme
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1662 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- local/share/perl/site_perl/Hebcal.pm
+++ local/share/perl/site_perl/Hebcal.pm
@@ -114,7 +114,7 @@ $Hebcal::havdalah_min = 72;
      'usa'     => 'USA, Mexico, Canada',
      'israel'  => 'Israel',
      'eu'      => 'European Union',
-     'aunz'    => 'Australia and New Zeland',
+     'aunz'    => 'Australia and NZ',
      );
 
 %Hebcal::cities =
