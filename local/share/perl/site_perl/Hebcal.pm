72e20809f2b56c35e86f05694519e9fdf2034a61
Michael J. Radwin <michael@radwin.org>
2004-08-25 17:09:58 +0000

    # on 2004/08/25 17:09:58, mradwin did: no auto-import
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1675 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- local/share/perl/site_perl/Hebcal.pm
+++ local/share/perl/site_perl/Hebcal.pm
@@ -43,12 +43,11 @@
 package Hebcal;
 require 5.000;
 use strict;
-use Time::Local;
 use CGI qw(-no_xhtml);
 use POSIX qw(strftime);
 use lib "/home/mradwin/local/share/perl";
 use lib "/home/mradwin/local/share/perl/site_perl";
-use Date::Calc;
+use Date::Calc ();
 use DB_File;
 
 ########################################################################
