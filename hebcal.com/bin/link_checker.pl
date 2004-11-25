51e805925dc4000dc6a436ecfe98ecd312d28197
Michael J. Radwin <michael@radwin.org>
2004-11-25 04:11:26 +0000

    # on 2004/11/25 04:11:26, mradwin did: Initial revision
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1775 eef4b989-ff24-4261-96ef-5f3e0244f1be

---
+++ hebcal.com/bin/link_checker.pl
@@ -0,0 +1,32 @@
+#!/usr/local/bin/perl -w
+
+use LWP::UserAgent;
+use strict;
+
+my $ua = LWP::UserAgent->new(keep_alive => 1, timeout => 30);
+
+my %checked;
+foreach my $f (@ARGV) {
+    open(F,$f) || die "$f: $!\n";
+    while(<F>) {
+	if (/href=\"(http[^\"]+)\"/) {
+	    my $url = $1;
+	    next if $url =~ /(www\.bible\.ort\.org|amazon\.com|hebcal\.com)/;
+	    next if $checked{$url};
+	    my $req = HTTP::Request->new('GET', $url);
+	    my $res = $ua->request($req);
+
+	    # Check the outcome of the response
+	    if (!$res->is_success) {
+		print $url, "\n";
+		print "\t", $res->status_line, "\n";
+	    }
+
+	    $checked{$url} = 1;
+	}
+    }
+    close(F);
+}
+
+
+
