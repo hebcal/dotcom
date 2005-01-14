c4422bad681797a2356ee7a3575bdcaccef625cb
Michael J. Radwin <michael@radwin.org>
2005-01-14 21:55:01 +0000

    # on 2005/01/14 21:55:01, mradwin did: caching infrastructure
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1871 eef4b989-ff24-4261-96ef-5f3e0244f1be

---
+++ hebcal.com/shabbat/index.php
@@ -0,0 +1,37 @@
+<?php
+# $Id$
+# $Source: /Users/mradwin/hebcal-copy/hebcal.com/shabbat/RCS/index.php,v $
+
+$qs = getenv("QUERY_STRING");
+$matches = array();
+if ($qs && preg_match('/\bcfg=([a-z])/', $qs, $matches)) {
+    $cfg = $matches[1];
+} else {
+    $cfg = "";
+}
+
+if ($cfg == "j") {
+    header("Content-Type: application/x-javascript");
+} elseif ($cfg == "r" ) {
+    header("Content-Type: text/xml");
+} elseif ($cfg == "w") {
+    header("Content-Type: text/vnd.wap.wml");
+} else {
+    header("Content-Type: text/html; charset=UTF-8");
+}
+
+$url = "/shabbat/shabbat.cgi";
+if ($qs) {
+    $url .= "?$qs";
+    $cqs = strtr($qs, "&;./", ",,_-");
+    $cqs = str_replace("%20", "+", $cqs);
+    $dir = getenv("DOCUMENT_ROOT") . "/cache/shabbat/shabbat_cgi";
+    $status = @readfile("$dir/$cqs");
+} else {
+    $status = false;
+}
+if (!$status) {
+    virtual($url);
+}
+exit();
+?>
