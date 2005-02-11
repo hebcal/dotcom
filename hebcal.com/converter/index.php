7a96a1eed04872d19c1780741b70d3fad7714640
Michael J. Radwin <michael@radwin.org>
2005-02-11 20:08:29 +0000

    # on 2005/02/11 20:08:29, mradwin did: Link to Wikipedia for Gregorian Reformation
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1947 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/converter/index.php
+++ hebcal.com/converter/index.php
@@ -194,9 +194,9 @@ if ($gy < 1752) {
 Results for year 1752 C.E. and before may not be accurate.</span>
 Hebcal does not take into account a correction of ten days that
 was introduced by Pope Gregory XIII known as the Gregorian
-Reformation. For more information, see <a
-href="http://www.xoc.net/maya/help/gregorian.asp">Gregorian and
-Julian Calendars</a>.</p>
+Reformation. For more information, read about the <a
+href="http://en.wikipedia.org/wiki/Gregorian_calendar#Adoption_outside_of_Roman_Catholic_nations">adoption
+of the Gregorian Calendar</a>.</p>
 <?php
 }
 
