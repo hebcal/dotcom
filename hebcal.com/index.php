6363117c8f5114537bc191878275360ef45da3c7
Michael J. Radwin <michael@radwin.org>
2005-05-09 19:34:58 +0000

    # on 2005/05/09 19:34:58, mradwin did: No more pesach greeting
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@1973 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/index.php
+++ hebcal.com/index.php
@@ -40,7 +40,6 @@ Jewish Calendar Tools</small></td>
 <h1>hebcal.com: Jewish Calendar Tools</h1>
 <span class="fpsubhead">
 <!-- holiday greeting -->
-<span class="fpgreeting">Chag Kasher v'Sameach!</span> &nbsp; - &nbsp;
 <!-- end holiday greeting -->
 <?php echo date("D, j F Y") ?> &nbsp; - &nbsp; <?php include("./today.inc") ?>
 <?php if ($rosh_chodesh) { ?>
