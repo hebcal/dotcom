ee069cc9f0c1df43e02e95149fe2303118276c03
Michael J. Radwin <michael@radwin.org>
2005-09-02 17:01:19 +0000

    # on 2005/09/02 17:01:19, mradwin did: remove html from gen_holidays
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@2002 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/gen_holidays.pl
+++ hebcal.com/bin/gen_holidays.pl
@@ -51,14 +51,14 @@ use strict;
 
 $0 =~ s,.*/,,;  # basename
 
-my($usage) = "usage: $0 [-h] holidays.ini holidays.html holidays.db
+my($usage) = "usage: $0 [-h] holidays.ini holidays.db
     -h        Display usage information.
 ";
 
 my(%opts);
 Getopt::Std::getopts('h', \%opts) || die "$usage\n";
 $opts{'h'} && die "$usage\n";
-(@ARGV == 3) || die "$usage";
+(@ARGV == 2) || die "$usage";
 
 my($this_year) = (localtime)[5];
 $this_year += 1900;
@@ -67,7 +67,6 @@ my($rcsrev) = '$Revision$'; #'
 $rcsrev =~ s/\s*\$//g;
 
 my($infile) = shift;
-my($outfile) = shift;
 my($outdb) = shift;
 
 my($holidays) = new Config::IniFiles(-file => $infile);
@@ -77,7 +76,6 @@ my(%DB);
 tie(%DB, 'DB_File', $outdb, O_RDWR|O_CREAT, 0644, $DB_HASH)
     or die;
 
-my(%out);
 foreach my $h ($holidays->Sections())
 {
     $DB{$h} = 1;
@@ -90,144 +88,8 @@ foreach my $h ($holidays->Sections())
 	if defined $holidays->val($h, 'descr');
     $DB{"$h:class"} = $holidays->val($h, 'class')
 	if defined $holidays->val($h, 'class');
-
-    next unless defined $holidays->val($h, 'ord') &&
-	defined $holidays->val($h, 'class');
-    $out{$holidays->val($h, 'class')}->{$holidays->val($h, 'ord')} = $h;
 }
-
-open(OUT, ">$outfile") || die "$outfile: $!\n";
-
-print OUT <<EOHTML;
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-	"http://www.w3.org/TR/html4/loose.dtd">
-<html><head><title>Hebcal Jewish Holidays</title>
-<base href="http://www.hebcal.com/help/holidays.html" target="_top">
-<link rel="stylesheet" href="/style.css" type="text/css">
-</head>
-<body>
-<!--htdig_noindex-->
-<table width="100%" class="navbar">
-<tr><td><small>
-<strong><a href="/">hebcal.com</a></strong> <tt>-&gt;</tt>
-<a href="/help/">Help</a> <tt>-&gt;</tt>
-Holidays
-</small></td>
-<td align="right"><small><a
-href="/search/">Search</a></small>
-</td></tr></table>
-
-<a name="top"></a>
-<!--/htdig_noindex-->
-<h1>Jewish Holidays</h1>
-EOHTML
-;
-
-my($strassfeld_link) =
-    "http://www.amazon.com/exec/obidos/ASIN/0062720082/ref=nosim/hebcal-20";
-print OUT qq{<h2>Major Holidays</h2>
-<p><a
-href="$strassfeld_link"><img
-src="/i/0062720082.01.TZZZZZZZ.jpg" border="0" hspace="5"
-alt="The Jewish Holidays: A Guide &amp; Commentary"
-vspace="5" width="75" height="90" align="right"></a>
-For a good reference book on the major Jewish Holidays, I'd suggest
-Michael Strassfeld's <em><a
-href="$strassfeld_link">The
-Jewish Holidays: A Guide &amp; Commentary</a></em>.</p>
-};
-&do_section('major');
-
-print OUT "\n<h3>Special Shabbatot</h3>\n";
-&do_section('shabbat');
-
-print OUT "\n<h3>Minor Fast Days</h3>\n";
-&do_section('fast');
-
-print OUT "\n<h3>New Holidays</h3>\n";
-&do_section('modern');
-
-print OUT "\n<h2>Rosh Chodesh</h2>\n";
-&do_section('rc');
-
-my($mtime) = (stat($infile))[9];
-my($hhmts) = "Last modified:\n" . localtime($mtime);
-my($copyright) = Hebcal::html_copyright2('',0,undef);
-
-print OUT <<EOHTML;
-
-<h2>Other Hebcal Holidays</h2>
-<dl>
-<dt><a name="omer"
-href="http://www.jewfaq.org/holidayb.htm">Days of the Omer</a>
-<dd>7 weeks from the second night of Pesach to the day before Shavuot
-</dl>
-
-<a name="faq"></a>
-<h2>Frequently Asked Questions</h2>
-
-<h3><a name="begin">When do the Holidays begin?</a></h3>
-
-<p>All Jewish Holidays begin the evening before the date specified.  This
-is because the Jewish day actually begins at sundown on the previous
-night.  Sometimes, for clarity, the Erev Holiday is also included.</p>
-
-<h3><a name="shabbat">When does Shabbat begin?</a></h3>
-
-<p>Shabbat begins 18 minutes before sundown on Friday night.  In Jerusalem,
-Shabbat begins 40 minutes before sundown.</p>
-
-<h3><a name="chol">What does CH''M mean?</a></h3>
-
-<p><strong>CH''M</strong> is an abbreviation for Chol Ha-Mo'ed.  Chol
-Ha-Mo'ed are the intermediate days of Passover and Sukkot, when work is
-permitted.</p>
-
-<p>
-<hr noshade size="1">
-<span class="tiny">$copyright
-<br>
-$hhmts
-($rcsrev)
-</span>
-</body></html>
-EOHTML
-;
-
-close(OUT);
 
 untie(%DB);
 
 exit(0);
-
-sub do_section
-{
-    my($class) = @_;
-
-    print OUT "<dl>\n";
-    foreach my $ord (sort {$a <=> $b} keys %{$out{$class}})
-    {
-	my($h) = $out{$class}->{$ord};
-	my($anchor) = $holidays->val($h, 'anchor');
-	my($href) = $holidays->val($h, 'href');
-	my($descr) = $holidays->val($h, 'descr');
-
-	if (defined $href) {
-	    $href = "\nhref=\"$href\"";
-	} else {
-	    $href = '';
-	}
-
-	if (defined $descr) {
-	    $descr = "<dd>$descr\n";
-	} else {
-	    $descr = '';
-	}
-
-	$h =~ s/\s+I$//;	# Pesach I, Sukkot I, etc.
-	$h =~ s/:\s+\d+.+$//;	# Chanukah: 1 Candle
-
-	print OUT qq{<dt><a name="$anchor"$href>$h</a>\n$descr};
-    }
-    print OUT "</dl>\n";
-}
