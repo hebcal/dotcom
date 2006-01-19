e0c5b6531e4163f20f00241669803a0654de4be7
Michael J. Radwin <michael@radwin.org>
2006-01-19 22:20:26 +0000

    # on 2006/01/19 22:20:26, hebcal did: new hebcal user
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@2093 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/cache_cleanup.sh
+++ hebcal.com/bin/cache_cleanup.sh
@@ -3,7 +3,7 @@
 # $Id$
 # $Source: /Users/mradwin/hebcal-copy/hebcal.com/bin/RCS/cache_cleanup.sh,v $
 
-HEBCAL_WEB=/home/mradwin/web/hebcal.com
+HEBCAL_WEB=/home/hebcal/web/hebcal.com
 
 if [ -d $HEBCAL_WEB/cache ]; then
     /bin/mv -f $HEBCAL_WEB/cache $HEBCAL_WEB/cache.old
