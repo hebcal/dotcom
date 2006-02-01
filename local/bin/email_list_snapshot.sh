c0c1d5bdb133e5bcb5e0f80ac110cc98adf70c9d
Michael J. Radwin <michael@radwin.org>
2006-02-01 17:52:37 +0000

    # on 2006/02/01 17:52:37, hebcal did: HCHOME
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@2130 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- local/bin/email_list_snapshot.sh
+++ local/bin/email_list_snapshot.sh
@@ -40,11 +40,12 @@
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ########################################################################
 
+HCHOME=/home/hebcal
 /usr/bin/mysqldump \
   --skip-opt \
   --host=mysql.hebcal.com --user=mradwin_hebcal --password=xxxxxxxx \
   hebcal1 hebcal_shabbat_email \
-  > $HOME/local/etc/hebcalsubs.sql.tmp && \
-/bin/mv $HOME/local/etc/hebcalsubs.sql.tmp $HOME/local/etc/hebcalsubs.sql && \
-nice /usr/bin/ci -q -m'daily checkin' -l $HOME/local/etc/hebcalsubs.sql
+  > $HCHOME/local/etc/hebcalsubs.sql.tmp && \
+/bin/mv $HCHOME/local/etc/hebcalsubs.sql.tmp $HCHOME/local/etc/hebcalsubs.sql && \
+nice /usr/bin/ci -q -m'daily checkin' -l $HCHOME/local/etc/hebcalsubs.sql
 
