f11a9c0a04403739688c7d9f4f03107a925cca19
Michael J. Radwin <michael@radwin.org>
2006-08-08 06:05:00 +0000

    # on 2006/08/08 06:05:00, mradwin did: almost working
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@2238 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/email/verify.php
+++ hebcal.com/email/verify.php
@@ -1,93 +1,15 @@
 <?php
 // $Id$
 // $Source: /Users/mradwin/hebcal-copy/hebcal.com/email/RCS/verify.php,v $
-header("Cache-Control: private");
-
-$param = array();
-foreach($_REQUEST as $key => $value) {
-    $param[$key] = trim($value);
-}
-
-
-if (isset($_SERVER["QUERY_STRING"]) &&
-    strlen($_SERVER["QUERY_STRING"]) == 24) {
-    $info = get_sub_info($_SERVER["QUERY_STRING"]);
-} else {
-    header("HTTP/1.0 400 Bad Request");
-    echo "Missing QUERY_STRING";
-    exit(0);
-}
-
-if (!isset($info["em"])) {
-    header("HTTP/1.0 404 Not Found");
-    echo "Can't find $_SERVER[QUERY_STRING] in DB";
-    exit(0);
-}
 
 require "../pear/Hebcal/smtp.inc";
 require "../pear/Hebcal/common.inc";
-require "../pear/HTML/Form.php";
 
-$VER = '$Revision$';
-$matches = array();
-if (preg_match('/(\d+)\.(\d+)/', $VER, $matches)) {
-    $VER = $matches[1] . "." . $matches[2];
+function bad_request($err) {
+    header("HTTP/1.0 400 Bad Request");
+    echo "$err\n";
+    exit(0);
 }
-?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-	"http://www.w3.org/TR/html4/loose.dtd">
-<html lang="en">
-<head><title>Confirm Email Subscription</title>
-<base href="http://www.hebcal.com/email/verify" target="_top">
-<link type="text/css" rel="stylesheet" href="/style.css">
-</head><body><table width="100%"
-class="navbar"><tr><td><small><strong><a
-href="/">hebcal.com</a></strong>
-<tt>-&gt;</tt>
-<a href="/shabbat/">1-Click Shabbat</a>
-<tt>-&gt;</tt>
-Email</small></td><td align="right"><small><a
-href="/help/">Help</a> -
-<a href="/search/">Search</a></small>
-</td></tr></table><h1>Confirm Email Subscription</h1>
-<?php
-
-if ($param["v"])
-{
-    $email = $param["em"];
-    if (!$email)
-    {
-	form($param,
-	     "Please enter your email address.");
-    }
-
-    $to_addr = email_address_valid($email);
-    if ($to_addr == false) {
-	form($param,
-	     "Sorry, <b>" . htmlspecialchars($email) . "</b> does\n" .
-	     "not appear to be a valid email address.");
-    }
-
-    // email is OK, write canonicalized version
-    $email = $to_addr;
-    
-    $param["em"] = strtolower($email);
-}
-else
-{
-    form($param);
-}
-
-if ($param["modify"]) {
-    subscribe($param);
-}
-elseif ($param["unsubscribe"]) {
-    unsubscribe($param);
-}
-else {
-    form($param);
-}
-my_footer();
 
 function get_password() {
     $passfile = file("../hebcal-db-pass.cgi");
@@ -99,32 +21,6 @@ function my_open_db() {
     $db = mysql_pconnect("mysql.hebcal.com", "mradwin_hebcal", get_password())
 	or die("Could not connect: " . mysql_error());
     return $db;
-}
-
-function write_sub_info($param) {
-    $db = my_open_db();
-
-    if ($param["geo"] == "zip")
-    {
-	$geo_sql = "email_candles_zipcode='$param[zip]',email_candles_city=NULL";
-    }
-    else if ($param["geo"] == "city")
-    {
-	$geo_sql = "email_candles_city='$param[city]',email_candles_zipcode=NULL";
-    }
-
-    $optin_announce = $param["upd"] ? 1 : 0;
-
-    $sql = <<<EOD
-UPDATE hebcal1.hebcal_shabbat_email
-SET email_status='active',
-    $geo_sql,
-    email_candles_havdalah='$param[m]',
-    email_optin_announce='$optin_announce'
-WHERE email_address = '$param[em]'
-EOD;
-
-    return mysql_query($sql, $db);
 }
 
 function get_sub_info($id) {
@@ -163,66 +59,6 @@ EOD;
     return $val;
 }
 
-function write_staging_info($param, $old_encoded)
-{
-    if ($old_encoded)
-    {
-	$encoded = $old_encoded;
-    }
-    else
-    {
-	$now = time();
-	$rand = pack("V", $now);
-
-	if ($_SERVER["REMOTE_ADDR"])
-	{
-	    list($p1,$p2,$p3,$p4) = explode(".", $_SERVER["REMOTE_ADDR"]);
-	    $rand .= pack("CCCC", $p1, $p2, $p3, $p4);
-	}
-
-	# As of PHP 4.2.0, there is no need to seed the random 
-	# number generator as this is now done automatically.
-	$rand .= pack("V", rand());
-
-	$encoded = bin2hex($rand);
-    }
-
-    $db = my_open_db();
-
-    if ($param["geo"] == "zip")
-    {
-	$location_name = "email_candles_zipcode";
-	$location_value = $param["zip"];
-    }
-    else if ($param["geo"] == "city")
-    {
-	$location_name = "email_candles_city";
-	$location_value = $param["city"];
-    }
-
-    $optin_announce = $param["upd"] ? 1 : 0;
-
-    $sql = <<<EOD
-REPLACE INTO hebcal1.hebcal_shabbat_email
-(email_id, email_address, email_status, email_created,
- email_candles_havdalah, email_optin_announce,
- $location_name, email_ip)
-VALUES ('$encoded', '$param[em]', 'pending', NOW(),
-	'$param[m]', '$optin_announce',
-	'$location_value', '$_SERVER[REMOTE_ADDR]')
-EOD;
-
-    $result = mysql_query($sql, $db)
-	or die("Invalid query 2: " . mysql_error());
-
-    if (mysql_affected_rows($db) < 1) {
-	die("Strange numrows from MySQL:" . mysql_error());
-    }
-
-    return $encoded;
-}
-
-
 function my_footer() {
     $stat = stat($_SERVER["SCRIPT_FILENAME"]);
     $year = strftime("%Y", time());
@@ -249,91 +85,125 @@ EOD
     exit();
 }
 
-function form($param, $message = "", $help = "") {
-    if ($message != "") {
-	$message = '<hr noshade size="1"><p><font' . "\n" .
-	    'color="#ff0000">' .  $message . "</font></p>" . $help . 
-	    '<hr noshade size="1">';
-    }
+$VER = '$Revision$';
+$matches = array();
+if (preg_match('/(\d+)\.(\d+)/', $VER, $matches)) {
+    $VER = $matches[1] . "." . $matches[2];
+}
 
-    echo $message;
+header("Cache-Control: private");
 
-    if (!$param["dst"]) {
-	$param["dst"] = "usa";
-    }
-    if (!$param["tz"]) {
-	$param["tz"] = "auto";
-    }
-    if (!isset($param["m"])) {
-	$param["m"] = 72;
-    }
+$param = array();
+foreach($_REQUEST as $key => $value) {
+    $param[$key] = trim($value);
+}
 
+if (isset($param["k"])) {
+    if (!preg_match('/^[0-9a-f]{24}$/', $param["k"])) {
+	bad_request("Invalid confirmation key");
+    }
+    $info = get_sub_info($param["k"]);
+} elseif (isset($_SERVER["QUERY_STRING"]) &&
+	  preg_match('/^[0-9a-f]{24}$/', $_SERVER["QUERY_STRING"])) {
+    $info = get_sub_info($_SERVER["QUERY_STRING"]);
+} else {
+    bad_request("No confirmation key");
+    exit(0);
+}
+
+if (!isset($info["em"])) {
+    header("HTTP/1.0 404 Not Found");
+    echo "Can't find $_SERVER[QUERY_STRING] in DB";
+    exit(0);
+}
+
+if (isset($param["commit"]) && $param["commit"] == "1") {
+    $db = my_open_db();
+    $sql = <<<EOD
+UPDATE hebcal1.hebcal_shabbat_email
+SET email_status='active',
+    email_ip='$_SERVER[REMOTE_ADDR]'
+WHERE email_id = '$info[id]'
+EOD;
+
+    mysql_query($sql, $db)
+	or die("Invalid query 2: " . mysql_error());
 ?>
-<p>Fill out the form to subscribe to weekly Shabbat candle
-lighting times and Torah portion.
-<br>Email is sent out every week on Thursday morning.</p>
-
-<form name="f1" id="f1" action="/email/tmp.php" method="post">
-
-<?php if (isset($param["geo"]) && $param["geo"] == "city") { ?>
-<input type="hidden" name="geo" value="city">
-<label for="city">Closest City:</label>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head><title>Email Subscription Confirmed</title>
+<base href="http://www.hebcal.com/email/verify.php" target="_top">
+<link type="text/css" rel="stylesheet" href="/style.css">
+</head><body><table width="100%"
+class="navbar"><tr><td><small><strong><a
+href="/">hebcal.com</a></strong>
+<tt>-&gt;</tt>
+<a href="/shabbat/">1-Click Shabbat</a>
+<tt>-&gt;</tt>
+Email</small></td><td align="right"><small><a
+href="/help/">Help</a> -
+<a href="/search/">Search</a></small>
+</td></tr></table><h1>Email Subscription Confirmed</h1>
+<p>Thank you for your interest in weekly
+candle lighting times and parsha information.</p>
+<p>A confirmation message has been sent
+to <b><?php echo htmlentities($info["em"]) ?></b>.</p>
 <?php
-global $hebcal_city_tz;
-$entries = array();
-foreach ($hebcal_city_tz as $k => $v) {
-    $entries[$k] = $k;
-}
-if ($param["city"]) {
-    $geo_city = htmlspecialchars($param["city"]);
-}
-echo HTML_Form::returnSelect("city", $entries,
-			     $geo_city ? $geo_city : "Jerusalem", 1,
-			     "", false, 'id="city"');
+     my_footer();
+} else {
+    if (isset($info["zip"]) && preg_match('/^\d{5}$/', $info["zip"])) {
+	$password = get_password();
+	list($long_deg,$long_min,$lat_deg,$lat_min,$tz,$dst,$city,$state) =
+	    hebcal_get_zipcode_fields($info["zip"], $password);
+	$city_descr = "$city, $state " . $info["zip"];
+	global $hebcal_tz_names;
+	$info["tz"] = $tz;
+	$tz_descr = "Time zone: " . $hebcal_tz_names["tz_" . $tz];
+	$dst_descr = "Daylight Saving Time: " . ($dst ? "usa" : "none");
+	unset($info["city"]);
+    } else {
+	$city_descr = $info["city"];
+	global $hebcal_tz_names;
+	$tz_descr = "Time zone: " .
+	     $hebcal_tz_names["tz_" . $hebcal_city_tz[$info["city"]]];
+	$dst_descr = "";
+	unset($info["zip"]);
+    }
 ?>
-&nbsp;&nbsp;<small>(or select by <a
-href="/email/?geo=zip">zip code</a></small>)
-<?php } else { ?>
-<input type="hidden" name="geo" value="zip">
-<label for="zip">Zip code:
-<input type="text" name="zip" size="5" maxlength="5" id="zip"
-value="<?php echo htmlspecialchars($param["zip"]) ?>"></label>
-&nbsp;&nbsp;<small>(or select by <a
-href="/email/?geo=city">closest city</a></small>)
-<?php } ?>
-
-<br><label for="m1">Havdalah minutes past sundown:
-<input type="text" name="m" value="<?php
-  echo htmlspecialchars($param["m"]) ?>" size="3" maxlength="3" id="m1">
-</label>
-
-<br><label for="em">E-mail address:
-<input type="text" name="em" size="30"
-value="<?php echo htmlspecialchars($param["em"]) ?>" id="em">
-</label>
-
-<br><label for="upd">
-<input type="checkbox" name="upd" value="on" <?php
-  if ($param["upd"] == "on") { echo "checked"; } ?> id="upd">
-Contact me occasionally about changes to the hebcal.com website.
-</label>
-
-<br>
-<input type="hidden" name="v" value="1">
-<?php global $is_update;
-    if ($is_update) { ?>
-<input type="hidden" name="prev"
-value="<?php echo htmlspecialchars($param["em"]) ?>">
-<?php } ?> 
-<br>
-<input type="submit" name="modify" value="<?php
-  echo ($is_update) ? "Modify Subscription" : "Subscribe"; ?>">
-or
-<input type="submit" name="unsubscribe" value="Unsubscribe">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head><title>Confirm Email Subscription</title>
+<base href="http://www.hebcal.com/email/verify.php" target="_top">
+<link type="text/css" rel="stylesheet" href="/style.css">
+</head><body><table width="100%"
+class="navbar"><tr><td><small><strong><a
+href="/">hebcal.com</a></strong>
+<tt>-&gt;</tt>
+<a href="/shabbat/">1-Click Shabbat</a>
+<tt>-&gt;</tt>
+Email</small></td><td align="right"><small><a
+href="/help/">Help</a> -
+<a href="/search/">Search</a></small>
+</td></tr></table><h1>Confirm Email Subscription</h1>
+<?php
+	$html = <<<EOD
+<p>Please confirm your subscription to hebcal.com weekly Shabbat Candle
+Lighting Times.</p>
+<p>Email: <b>$info[em]</b>
+<br>Location: $city_descr
+<br><small>&nbsp;&nbsp;$tz_descr
+<br>&nbsp;&nbsp;$dst_descr
+</small></p>
+<p>
+<form method="post" name="f1" id="f1" action="/email/verify.php">
+<input type="hidden" name="k" value="$info[id]">
+<input type="hidden" name="commit" value="1">
+<input type="submit" name="sub1" id="sub1" value="Confirm Subscription">
 </form>
-
-<p><hr noshade size="1">
-<h3><a name="privacy">Email Privacy Policy</a></h3>
+</p>
+<h4>Email Privacy Policy</h4>
 
 <p>We will never sell or give your email address to anyone.
 <br>We will never use your email address to send you unsolicited
@@ -342,263 +212,10 @@ offers.</p>
 <p>To unsubscribe, send an email to <a
 href="mailto:shabbat-unsubscribe&#64;hebcal.com">shabbat-unsubscribe&#64;hebcal.com</a>.</p>
 
-<?php
-    my_footer();
-}
-
-function subscribe($param) {
-    global $sender, $VER;
-    if (preg_match('/\@hebcal.com$/', $param["em"]))
-    {
-	form($param,
-	     "Sorry, can't use a <b>hebcal.com</b> email address.");
-    }
-
-    if ($param["geo"] == "zip")
-    {
-	if (!$param["zip"])
-	{
-	    form($param,
-	    "Please enter your zip code for candle lighting times.");
-	}
-
-	if (!$param["dst"]) {
-	    $param["dst"] = "usa";
-	}
-	if (!$param["tz"]) {
-	    $param["tz"] = "auto";
-	}
-
-	if (!preg_match('/^\d{5}$/', $param["zip"]))
-	{
-	    form($param,
-	    "Sorry, <b>" . $param["zip"] . "</b> does\n" .
-	    "not appear to be a 5-digit zip code.");
-	}
-
-	$password = get_password();
-	list($long_deg,$long_min,$lat_deg,$lat_min,$tz,$dst,$city,$state) =
-	    hebcal_get_zipcode_fields($param["zip"], $password);
-
-	if (!$state)
-	{
-	    form($param,
-	    "Sorry, can't find\n".  "<b>" . $param["zip"] .
-	    "</b> in the zip code database.\n",
-	    "<ul><li>Please try a nearby zip code</li></ul>");
-	}
-
-	$city_descr = "$city, $state " . $param["zip"];
-
-	// handle timezone == "auto"
-	if ($tz == "?" || $tz == "0")
-	{
-	    form($param,
-	    "Sorry, can't auto-detect\n" .
-	    "timezone for <b>" . $city_descr . "</b>\n",
-	    "<ul><li>Please select your time zone below.</li></ul>");
-	}
-
-	global $hebcal_tz_names;
-	$param["tz"] = $tz;
-	$tz_descr = "Time zone: " . $hebcal_tz_names["tz_" . $tz];
-
-	if ($dst) {
-	    $param["dst"] = "usa";
-	} else {
-	    $param["dst"] = "none";
-	}
-
-	$dst_descr = "Daylight Saving Time: " . $param["dst"];
-
-	unset($param["city"]);
-    }
-    else if ($param["geo"] == "city")
-    {
-	if (!$param["city"])
-	{
-	    form($param,
-	    "Please select a city for candle lighting times.");
-	}
-
-	global $hebcal_city_tz;
-	if (!isset($hebcal_city_tz[$param["city"]]))
-	{
-	    form($param,
-	    "Sorry, <b>" . htmlspecialchars($param["city"]) . "</b> is\n" .
-	    "not a recoginized city.");
-	}
-
-	$city_descr = $param["city"];
-	global $hebcal_tz_names;
-	$tz_descr = "Time zone: " .
-	     $hebcal_tz_names["tz_" . $hebcal_city_tz[$param["city"]]];
-	$dst_descr = "";
-
-	unset($param["zip"]);
-    }
-    else
-    {
-	$param["geo"] = "zip";
-	form($param, "Sorry, missing zip or city field.");
-    }
-
-    # check for old sub
-    if (isset($param["prev"]) && $param["prev"] != $param["em"]) {
-	$info = get_sub_info($param["prev"]);
-	if (isset($info["status"]) && $info["status"] == "active") {
-	    sql_unsub($param["prev"]);
-	}
-    }
-
-    # check if email address already verified
-    $info = get_sub_info($param["em"]);
-    if (isset($info["status"]) && $info["status"] == "active")
-    {
-	write_sub_info($param);
-
-	$from_name = "Hebcal Subscription Notification";
-    	$from_addr = "shabbat-owner@hebcal.com";
-	$return_path = "shabbat-return-" . strtr($param["em"], "@", "=") .
-	    "@hebcal.com";
-	$subject = "Your subscription is updated";
-
-	$ip = $_SERVER["REMOTE_ADDR"];
-
-	$headers = array("From" => "\"$from_name\" <$from_addr>",
-			 "To" => $param["em"],
-			 "Reply-To" => $from_addr,
-			 "List-Unsubscribe" =>
-			 "<mailto:shabbat-unsubscribe@hebcal.com>",
-			 "MIME-Version" => "1.0",
-			 "Content-Type" => "text/plain",
-			 "X-Sender" => $sender,
-			 "X-Mailer" => "hebcal web v$VER",
-			 "Message-ID" =>
-			 "<Hebcal.Web.$VER.".time().".".posix_getpid()."@hebcal.com>",
-			 "X-Originating-IP" => "[$ip]",
-			 "Subject" => $subject);
-
-	$body = <<<EOD
-Hello,
-
-We have updated your weekly Shabbat candle lighting time
-subscription for $city_descr.
-
-Regards,
-hebcal.com
-
-To unsubscribe from this list, send an email to:
-shabbat-unsubscribe@hebcal.com
-EOD;
-
-	$err = smtp_send($return_path, $param["em"], $headers, $body);
-
-	$html = <<<EOD
-<h2>Subscription Updated</h2>
-<p>Your subsciption information has been updated successfully.</p>
-<p><small>
-$city_descr
-<br>&nbsp;&nbsp;$tz_descr
-<br>&nbsp;&nbsp;$dst_descr
-</small></p>
 EOD
 	     ;
 
-	echo $html;
-	return true;
-    }
-
-    if (isset($info["status"]) && $info["status"] == "pending" &&
-	isset($info["id"]))
-    {
-	$old_encoded = $info["id"];
-    }
-    else
-    {
-	$old_encoded = null;
-    }
-
-    $encoded = write_staging_info($param, $old_encoded);
-
-    $from_name = "Hebcal Subscription Notification";
-    $return_path = "shabbat-return-" . strtr($param["em"], "@", "=") .
-	"@hebcal.com";
-    $from_addr = "no-reply@hebcal.com";
-    $subject = "Please confirm your request to subscribe to hebcal";
-
-    $ip = $_SERVER["REMOTE_ADDR"];
-
-    $headers = array("From" => "\"$from_name\" <$from_addr>",
-		     "To" => $param["em"],
-		     "MIME-Version" => "1.0",
-		     "Content-Type" => "text/plain",
-		     "X-Sender" => $sender,
-		     "X-Mailer" => "hebcal web v$VER",
-		     "Message-ID" =>
-		     "<Hebcal.Web.$VER.".time().".".posix_getpid()."@hebcal.com>",
-		     "X-Originating-IP" => "[$ip]",
-		     "Subject" => $subject);
-
-    $body = <<<EOD
-Hello,
-
-We have received your request to receive weekly Shabbat
-candle lighting time information from hebcal.com for
-$city_descr.
-
-Please confirm your request by clicking on this link:
-
-http://www.hebcal.com/email/verify?$encoded
-
-If you did not request (or do not want) weekly Shabbat
-candle lighting time information, please accept our
-apologies and ignore this message.
-
-Regards,
-hebcal.com
-
-[$_SERVER[REMOTE_ADDR]]
-EOD;
-
-    $err = smtp_send($return_path, $param["em"], $headers, $body);
-    $html_email = htmlentities($param["em"]);
-
-    if ($err === true)
-    {
-	$html = <<<EOD
-<p>Thank you for your interest in weekly
-candle lighting times and parsha information.</p>
-<p>A confirmation message has been sent
-to <b>$html_email</b>.<br>
-Simply reply to that message to confirm your subscription.</p>
-<p>If you do not receive this acknowledgment message within an hour
-or two, then the most likely problem is that you made a typo
-in your email address.  If you do not get the confirmation message,
-please return to the subscription page and try again, taking care
-to avoid typos.</p>
-<p><small>
-$city_descr
-<br>&nbsp;&nbsp;$tz_descr
-<br>&nbsp;&nbsp;$dst_descr
-</small></p>
-EOD
-		     ;
-    }
-    else
-    {
-	$html = <<<EOD
-<h2>Sorry!</h2>
-<p>Unfortunately, we are temporarily unable to send email
-to <b>$html_email</b>.</p>
-<p>Please try again in a few minutes.</p>
-<p>If the problem persists, please send email to
-<a href="mailto:webmaster&#64;hebcal.com">webmaster&#64;hebcal.com</a>.</p>
-EOD
-	     ;
-    }
-
-    echo $html;
+ echo $html;
+ my_footer();
 }
-
 ?>
