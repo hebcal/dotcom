ac572e716266ade79baaf6021591ef21b5884748
Michael J. Radwin <michael@radwin.org>
2006-08-08 04:46:11 +0000

    # on 2006/08/08 04:46:11, mradwin did: add ip addr
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@2236 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/hebcal.sql
+++ hebcal.com/bin/hebcal.sql
@@ -1,6 +1,6 @@
 create table hebcal1.hebcal_shabbat_email (
     email_address varchar(200) not null primary key,
-    email_id varchar(24) not null,
+    email_id char(24) not null,
     email_status varchar(16) not null,
     email_created datetime not null,
     email_updated timestamp,
@@ -8,6 +8,7 @@ create table hebcal1.hebcal_shabbat_email (
     email_candles_city varchar(20),
     email_candles_havdalah tinyint,
     email_optin_announce tinyint(1),
+    email_ip varchar(16),
     unique (email_id),
     index (email_status)
 );
