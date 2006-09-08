57617311bcaa1c85dba7ad04da64e52a741843c3
Michael J. Radwin <michael@radwin.org>
2006-09-08 16:33:30 +0000

    # on 2006/09/08 16:33:30, mradwin did: "Rosh Hashanah" instead of Rosh Hashana
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@2257 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/index.php
+++ hebcal.com/index.php
@@ -230,7 +230,7 @@ style="padding-left: 10px; padding-right: 5px">
 <ul class="gtl">
 <!-- Begin temp holiday -->
 <li><b><a
-href="/holidays/rosh-hashana.html?tag=fp.tmp">Rosh&nbsp;Hashana&nbsp;5767</a></b><br>22
+href="/holidays/rosh-hashana.html?tag=fp.tmp">Rosh&nbsp;Hashanah&nbsp;5767</a></b><br>22
 September 2006<br>at sundown
 <br><br>
 <!-- End temp holiday -->
