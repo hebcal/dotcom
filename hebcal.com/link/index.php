63026062d88548b36d1367f4067d7b965ba4129c
Michael J. Radwin <michael@radwin.org>
2006-09-10 16:25:57 +0000

    # on 2006/09/10 16:25:57, mradwin did: hebcal 3.7
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@2263 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/link/index.php
+++ hebcal.com/link/index.php
@@ -235,7 +235,7 @@ Michael J. Radwin. All rights reserved.
 <a target="_top" href="http://www.hebcal.com/news/">News</a> -
 <a target="_top" href="http://www.hebcal.com/donations/">Donate</a>
 <br>This website uses <a href="http://sourceforge.net/projects/hebcal/">hebcal
-3.5 for UNIX</a>, Copyright &copy; 2006 Danny Sadinoff. All rights reserved.
+3.7 for UNIX</a>, Copyright &copy; 2006 Danny Sadinoff. All rights reserved.
 <br>Software last updated: $date (Revision: $VER) 
 </span>
 </body></html>
