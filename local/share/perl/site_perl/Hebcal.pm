200c1315d5fa80ecba9d07f469d263ba1246e5f8
Michael J. Radwin <michael@radwin.org>
2006-11-21 21:26:08 +0000

    # on 2006/11/21 21:26:08, mradwin did: Add Google Analytics
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@2387 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- local/share/perl/site_perl/Hebcal.pm
+++ local/share/perl/site_perl/Hebcal.pm
@@ -1007,6 +1007,13 @@ sub html_footer
 3.7 for UNIX</a>, Copyright &copy; 2006 Danny Sadinoff. All rights reserved.
 <br>$hhmts ($rcsrev)
 </span>
+<script src="http://www.google-analytics.com/urchin.js"
+type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-967247-1";
+urchinTracker();
+</script>
 };
 
     if ($noclosebody) {
