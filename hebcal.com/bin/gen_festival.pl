078085c3176159e017dc0f2df92548ab437bfc76
Michael J. Radwin <michael@radwin.org>
2007-07-23 02:30:08 +0000

    # on 2007/07/23 02:30:08, mradwin did: 5768 calendar
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@2651 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/gen_festival.pl
+++ hebcal.com/bin/gen_festival.pl
@@ -257,11 +257,11 @@ href="/search/">Search</a></td></tr></table>
 <!--/htdig_noindex-->
 <a name="top"></a>
 <h1>Jewish Holidays</h1>
-<a title="Jewish Year 5767 from Amazon.com"
-href="http://www.amazon.com/o/ASIN/0789314495/hebcal-20"><img
-src="http://www.hebcal.com/i/0789314495.01.TZZZZZZZ.jpg" border="0"
-width="110" height="109" hspace="8" vspace="8" align="right"
-alt="Jewish Year 5767 from Amazon.com"></a>
+<a title="Jewish Year 5768 from Amazon.com"
+href="http://www.amazon.com/o/ASIN/0789316366/hebcal-20"><img
+src="http://www.hebcal.com/i/0789316366.01.TZZZZZZZ.jpg" border="0"
+width="110" height="110" hspace="8" vspace="8" align="right"
+alt="Jewish Year 5768 from Amazon.com"></a>
 <dl>
 EOHTML
 ;
