6f941685e5a059c81a87b52748dfb15404727313
Michael J. Radwin <michael@radwin.org>
2008-11-28 07:27:46 +0000

    backup to amazon S3
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@2781 eef4b989-ff24-4261-96ef-5f3e0244f1be

---
+++ local/bin/s3backup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+PATH=/bin:/usr/bin
+S3SYNCDIR=/home/mradwin/local/s3sync
+LOCKFILE=/tmp/mradwin.s3backup.lock
+BACKUPDIR=/home/hebcal/local/lib/svn
+
+dotlockfile -r 0 -p $LOCKFILE
+if [ $? != 0 ]; then
+   echo "s3backup is still running; exiting"
+   exit 1
+fi
+
+AWS_ACCESS_KEY_ID=xxxxxxxxxxxxxxxxxxxx
+AWS_SECRET_ACCESS_KEY=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx 
+
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+
+cd $S3SYNCDIR
+ruby s3sync.rb -r ${BACKUPDIR} hebcal:
+
