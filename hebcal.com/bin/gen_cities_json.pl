7efe50a1558e334e962568d02b93c3e6550cb560
Michael J. Radwin <michael@radwin.org>
2010-09-21 19:12:57 +0000

    country code to country name map
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@3013 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/gen_cities_json.pl
+++ hebcal.com/bin/gen_cities_json.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use utf8;
+use Locale::Country ();
 
 my $dir = $0;
 $dir =~ s,/[^/]+$,,;
@@ -16,7 +17,13 @@ binmode(CITIES, ":utf8");
 open(O,">$outfile.$$") || die "$outfile.$$: $!\n";
 binmode(O, ":utf8");
 
-print O "var hebcal_cities={\n";
+print O <<EOJS;
+if (typeof HEBCAL == "undefined" || !HEBCAL) {
+   var HEBCAL = {};
+}
+EOJS
+;
+print O "HEBCAL.cities={\n";
 my $first = 1;
 while(<CITIES>) {
     chomp;
@@ -27,6 +34,17 @@ while(<CITIES>) {
 }
 close(CITIES);
 
+print O "\n}\n";
+
+print O "HEBCAL.countries={\n";
+$first = 1;
+foreach my $cc (Locale::Country::all_country_codes()) {
+    my $name = Locale::Country::code2country($cc);
+    $name =~ s/,\s+.+$//;
+    print O ",\n" unless $first;
+    $first = 0;
+    printf O "\"%s\":\"%s\"", uc($cc), $name;
+}
 print O "\n}\n";
 
 close(O);
