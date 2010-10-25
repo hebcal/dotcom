c5470637dad1235694ebfc7dc4942efc20277d7c
Michael J. Radwin <michael@radwin.org>
2010-10-25 04:04:20 +0000

    update download links with classname/id
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@3143 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/ical/index.php
+++ hebcal.com/ical/index.php
@@ -31,24 +31,26 @@ for Jews living in the Diaspora (anywhere outside of modern Israel).</p>
 
 <?php
 function cal_row($path,$title,$subtitle) {
-    $webcal = "webcal://" . $_SERVER["HTTP_HOST"] . $path;
+    $url_noproto = $_SERVER["HTTP_HOST"] . "/ical/" . $path . ".ics";
+    $webcal = "webcal://" . $url_noproto;
     $webcal_esc = urlencode($webcal);
-    $http_esc = urlencode("http://" . $_SERVER["HTTP_HOST"] . $path);
+    $http_esc = urlencode("http://" . $url_noproto);
 ?>
 <tr>
-<td><a title="Subscribe to <?php echo $title ?> in iCal"
+<td><a class="download" id="quick-ical-<?php echo $path ?>"
+title="Subscribe to <?php echo $title ?> in iCal"
 href="<?php echo $webcal ?>"><img
 src="/i/ical-64x64.png" width="64" height="64"
 alt="Subscribe to <?php echo $title ?> in iCal"
 border="0"></a></td>
-<td align="center"><a
+<td align="center"><a class="download" id="quick-wlive-<?php echo $path ?>"
 title="Add <?php echo $title ?> to Windows Live Calendar"
 href="http://calendar.live.com/calendar/calendar.aspx?rru=addsubscription&url=<?php echo $webcal_esc ?>&name=<?php echo urlencode($title) ?>"><img
 src="/i/wlive-150x20.png"
 width="150" height="20" border="0"
-alt="Add <?php echo $title ?> to Windows Live Caledar"></a>
+alt="Add <?php echo $title ?> to Windows Live Calendar"></a>
 </td>
-<td align="center"><a
+<td align="center"><a class="download" id="quick-gcal-<?php echo $path ?>"
 title="Add <?php echo $title ?> to Google Calendar"
 href="http://www.google.com/calendar/render?cid=<?php echo $http_esc ?>"><img
 src="http://www.google.com/calendar/images/ext/gc_button6.gif"
@@ -63,13 +65,13 @@ alt="Add <?php echo $title ?> to Google Calendar"></a>
 ?>
 <table id="hebcal-ical" cellpadding="5">
 <?php
-cal_row("/ical/jewish-holidays.ics", "Jewish Holidays",
+cal_row("jewish-holidays", "Jewish Holidays",
 	"Major holidays such as Rosh Hashana, Yom Kippur, Passover, Hanukkah");
-cal_row("/ical/jewish-holidays-all.ics", "Jewish Holidays (all)",
+cal_row("jewish-holidays-all", "Jewish Holidays (all)",
 	"Also includes Rosh Chodesh, minor fasts, and special Shabbatot");
-cal_row("/ical/hdate-en.ics", "Hebrew calendar dates (English transliteration)",
+cal_row("hdate-en", "Hebrew calendar dates (English transliteration)",
 	"Displays the Hebrew date (such as <b>18th of Tevet, 5770</b>) every day of the week");
-cal_row("/ical/hdate-he.ics", "Hebrew calendar dates (Hebrew)",
+cal_row("hdate-he", "Hebrew calendar dates (Hebrew)",
 	"Displays the Hebrew date (such as <b>י״ח בטבת תש״ע</b>) every day of the week");
 ?>
 </table>
