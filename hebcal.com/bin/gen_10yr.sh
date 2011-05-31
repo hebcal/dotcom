b0384f8e80b75275b1f8e4e2b619be4a80a45a57
Michael J. Radwin <michael@radwin.org>
2011-05-31 20:24:47 +0000

    fetch csv urls, too
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@3207 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/gen_10yr.sh
+++ hebcal.com/bin/gen_10yr.sh
@@ -4,24 +4,28 @@ set -x
 
 TMPFILE=`mktemp`
 YEAR=`date +'%Y'`
+ICS_URL="http://www.hebcal.com/hebcal/index.cgi/export.ics"
+CSV_URL="http://www.hebcal.com/hebcal/index.cgi/hebcal_usa.csv"
 
-URL="http://www.hebcal.com/hebcal/index.cgi/export.ics?year=${YEAR};month=x;yt=G;v=1;nh=on;i=off;lg=s;vis=on;c=off;geo=zip;ny=10;nx=off;mf=off;ss=off"
-curl -o $TMPFILE $URL
-perl -pi -e "s/tag=ical/tag=dl-major/g" $TMPFILE
-cp $TMPFILE jewish-holidays.ics
+fetch_urls () {
+    file=$1
+    args=$2
+    curl -o $TMPFILE "${ICS_URL}?${args}" && cp $TMPFILE "${file}.ics"
+    curl -o $TMPFILE "${CSV_URL}?${args}" && cp $TMPFILE "${file}.csv"
+}
 
-URL="http://www.hebcal.com/hebcal/index.cgi/export.ics?year=${YEAR};month=x;yt=G;v=1;nh=on;i=off;lg=s;vis=on;c=off;geo=zip;ny=10;nx=on;mf=on;ss=on"
-curl -o $TMPFILE $URL
-perl -pi -e "s/tag=ical/tag=dl-all/g" $TMPFILE
-cp $TMPFILE jewish-holidays-all.ics
+FILE="jewish-holidays"
+fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;nh=on;i=off;lg=s;vis=on;c=off;geo=zip;ny=10;nx=off;mf=off;ss=off"
+perl -pi -e "s/tag=ical/tag=dl-major/g" "${FILE}.ics"
 
-URL="http://www.hebcal.com/hebcal/index.cgi/export.ics?year=${YEAR};month=x;yt=G;v=1;i=off;lg=s;vis=on;d=on;c=off;geo=zip;ny=5"
-curl -o $TMPFILE $URL && cp $TMPFILE hdate-en.ics
+FILE="jewish-holidays-all"
+fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;nh=on;i=off;lg=s;vis=on;c=off;geo=zip;ny=10;nx=on;mf=on;ss=on"
+perl -pi -e "s/tag=ical/tag=dl-all/g" "${FILE}.ics"
 
-URL="http://www.hebcal.com/hebcal/index.cgi/export.ics?year=${YEAR};month=x;yt=G;v=1;i=off;lg=h;vis=on;d=on;c=off;geo=zip;ny=5"
-curl -o $TMPFILE $URL && cp $TMPFILE hdate-he.ics
+fetch_urls "hdate-en" "year=${YEAR};month=x;yt=G;v=1;i=off;lg=s;vis=on;d=on;c=off;geo=zip;ny=5"
 
-URL="http://www.hebcal.com/hebcal/index.cgi/export.ics?year=${YEAR};month=x;yt=G;v=1;o=on;i=off;lg=s;vis=on;c=off;geo=zip;ny=10"
-curl -o $TMPFILE $URL && cp $TMPFILE omer.ics
+fetch_urls "hdate-he" "year=${YEAR};month=x;yt=G;v=1;i=off;lg=h;vis=on;d=on;c=off;geo=zip;ny=5"
+
+fetch_urls "omer" "year=${YEAR};month=x;yt=G;v=1;o=on;i=off;lg=s;vis=on;c=off;geo=zip;ny=10"
 
 rm -f $TMPFILE
