c9bbbfc7509fcaaeaa09e0f0253060d3208b7b61
Michael J. Radwin <michael@radwin.org>
2011-09-08 16:55:45 +0000

    text-only ad on homepage
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@3302 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/index.php
+++ hebcal.com/index.php
@@ -256,9 +256,9 @@ id="zip">&nbsp;<input type="submit" value="Go">
 </form>
 </li>
 <li id="advman-3" class="widget-container Advman_Widget"><h3 class="widget-title">Advertisement</h3><script type="text/javascript"><!--
-google_ad_client = "pub-7687563417622459";
-/* 200x200 */
-google_ad_slot = "8131407384";
+google_ad_client = "ca-pub-7687563417622459";
+/* 200x200 text only */
+google_ad_slot = "5114852649";
 google_ad_width = 200;
 google_ad_height = 200;
 //-->
