641b340d212360273ee0953833ff485183cc6661
Michael J. Radwin <michael@radwin.org>
2011-09-14 23:03:18 +0000

    include a description for SEO
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@3323 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/converter/index.php
+++ hebcal.com/converter/index.php
@@ -1,43 +1,43 @@
 <?php
 
-########################################################################
-# Convert between hebrew and gregorian calendar dates.
-#
-# Copyright (c) 2010  Michael J. Radwin.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or
-# without modification, are permitted provided that the following
-# conditions are met:
-#
-#  * Redistributions of source code must retain the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer.
-#
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials
-#    provided with the distribution.
-#
-#  * Neither the name of Hebcal.com nor the names of its
-#    contributors may be used to endorse or promote products
-#    derived from this software without specific prior written
-#    permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
-# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
-# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
-# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-########################################################################
+/***********************************************************************
+ * Convert between hebrew and gregorian calendar dates.
+ *
+ * Copyright (c) 2011  Michael J. Radwin.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ *  * Redistributions of source code must retain the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer.
+ *
+ *  * Redistributions in binary form must reproduce the above
+ *    copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials
+ *    provided with the distribution.
+ *
+ *  * Neither the name of Hebcal.com nor the names of its
+ *    contributors may be used to endorse or promote products
+ *    derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **********************************************************************/
 
 require("./HTML/Form.php");
 require("../pear/Hebcal/hebnum.inc");
@@ -185,13 +185,13 @@ if ($type == "g2h")
 {
     $first = "$dow$gd ". $MoY_long[$gm] .  " " . sprintf("%04d", $gy);
     $second = format_hebrew_date($hd, $hm, $hy);
-    my_header($second);
+    my_header($second, "$first = $second");
 }
 else
 {
     $first = format_hebrew_date($hd, $hm, $hy);
     $second = "$dow$gd ". $MoY_long[$gm] .  " " . sprintf("%04d", $gy);
-    my_header($first);
+    my_header($first, "$first = $second");
 }
 
 if ($gy < 1752) {
@@ -292,10 +292,14 @@ function display_hebrew_event($h) {
 }
 
 
-function my_header($hebdate) {
+function my_header($hebdate, $result = false) {
     header("Content-Type: text/html; charset=UTF-8");
-    $xtra_head =
-      "<link rel=\"alternate\" type=\"application/rss+xml\" title=\"RSS\" href=\"/etc/hdate-en.xml\">\n";
+    $description = $result ? " $result" : "";
+$xtra_head = <<<EOD
+<link rel="alternate" type="application/rss+xml" title="RSS" href="/etc/hdate-en.xml">
+<meta name="description" content="Convert between Gregorian/civil and Hebrew/Jewish calendar dates.$description">
+EOD;
+
     echo html_header_new("Hebrew Date Converter - $hebdate", $xtra_head);
 ?>
 <div id="container">
