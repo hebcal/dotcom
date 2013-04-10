2899f253c1c77da550ca2930b304a475a6e16f04
Michael J. Radwin <michael@radwin.org>
2013-04-10 20:32:57 +0000

    parsha haShavua ics feed
    git-svn-id: file:///home/hebcal/hebcal-dotcom-svn@3708 eef4b989-ff24-4261-96ef-5f3e0244f1be

--- hebcal.com/bin/gen_10yr.sh
+++ hebcal.com/bin/gen_10yr.sh
@@ -26,35 +26,41 @@ update_ics_name() {
 
 
 FILE="jewish-holidays"
-fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;nh=on;i=off;lg=s;vis=on;c=off;geo=zip;ny=10;nx=off;mf=off;ss=off"
+fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;nh=on;i=off;lg=s;c=off;geo=none;ny=10;nx=off;mf=off;ss=off"
 perl -pi -e "s/tag=ical/tag=dl-major/g" "${FILE}.ics"
 update_ics_name $FILE \
     "Jewish Holidays" \
     "http:\\/\\/www.hebcal.com\\/"
 
 FILE="jewish-holidays-all"
-fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;nh=on;i=off;lg=s;vis=on;c=off;geo=zip;ny=10;nx=on;mf=on;ss=on"
+fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;nh=on;i=off;lg=s;c=off;geo=none;ny=10;nx=on;mf=on;ss=on"
 perl -pi -e "s/tag=ical/tag=dl-all/g" "${FILE}.ics"
 update_ics_name $FILE \
     "Jewish Holidays" \
     "http:\\/\\/www.hebcal.com\\/"
 
 FILE="hdate-en"
-fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;i=off;lg=s;vis=on;d=on;c=off;geo=zip;ny=3"
+fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;i=off;lg=s;d=on;c=off;geo=none;ny=3"
 update_ics_name $FILE \
     "Hebrew calendar dates (en)" \
     "Displays the Hebrew date every day of the week in English transliteration"
 
 FILE="hdate-he"
-fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;i=off;lg=h;vis=on;d=on;c=off;geo=zip;ny=3"
+fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;i=off;lg=h;d=on;c=off;geo=none;ny=3"
 update_ics_name $FILE \
     "Hebrew calendar dates (he)" \
     "Displays the Hebrew date every day of the week in Hebrew"
 
 FILE="omer"
-fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;o=on;i=off;lg=s;vis=on;c=off;geo=zip;ny=3"
+fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;o=on;i=off;lg=s;c=off;geo=none;ny=3"
 update_ics_name $FILE \
-    "Hebcal Days of the Omer" \
+    "Days of the Omer" \
     "7 weeks from the second night of Pesach to the day before Shavuot"
+
+FILE="sedrot"
+fetch_urls $FILE "year=${YEAR};month=x;yt=G;v=1;s=on;i=off;lg=s;c=off;geo=none;ny=3"
+update_ics_name $FILE \
+    "Torah Readings (Diaspora)" \
+    "Parashat ha-Shavua - Weekly Torah Portion from Hebcal"
 
 rm -f $TMPFILE
