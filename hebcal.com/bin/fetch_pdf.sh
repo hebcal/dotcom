76a763d325d5c394311c95841196a528c88c4907
Michael J. Radwin <michael@radwin.org>
2013-07-05 09:03:50 -0700

    fetch some static PDF files

---
+++ hebcal.com/bin/fetch_pdf.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -x
+
+TMPFILE=`mktemp /tmp/tmp.XXXXXXXXXX`
+URL="http://www.hebcal.com/hebcal/index.cgi/export.pdf"
+ARGS="month=x&yt=H&v=1&nh=on&i=off&lg=s&c=off&geo=none&ny=1&nx=on&mf=on&ss=on&d=on"
+
+year=`date +'%Y'`
+for i in {1..6}
+do
+    hyear=$((year + i + 3759))
+    outfile="hebcal-${hyear}.pdf"
+    curl -o $TMPFILE "$URL?year=${hyear}&${ARGS}" && cp $TMPFILE $outfile
+    chmod 0644 $outfile
+done
+
+rm -f $TMPFILE
