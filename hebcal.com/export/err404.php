93bb2a8124726b424fdcfd7869bef447b52554df
Michael J. Radwin <michael@radwin.org>
2013-07-14 22:23:26 -0700

    get rid of stray _trackPageLoadTime

--- hebcal.com/export/err404.php
+++ hebcal.com/export/err404.php
@@ -52,7 +52,6 @@ header("Status: 404 Not Found");
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-967247-1']);
 _gaq.push(['_trackPageview']);
-_gaq.push(['_trackPageLoadTime']);
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
