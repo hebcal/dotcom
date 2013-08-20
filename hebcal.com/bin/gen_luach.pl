be876b1f4549fc7ed2422ecd2510542a2cc83b65
Michael J. Radwin <michael@radwin.org>
2013-08-20 13:31:14 -0700

    Google Analytics in footer

--- hebcal.com/bin/gen_luach.pl
+++ hebcal.com/bin/gen_luach.pl
@@ -479,6 +479,16 @@ href="http://creativecommons.org/licenses/by/3.0/deed.en_US">Creative Commons At
 
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0-wip/js/bootstrap.min.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-967247-3', 'reformluach.com');
+  ga('send', 'pageview');
+
+</script>
 </body></html>
 EOHTML
 ;
