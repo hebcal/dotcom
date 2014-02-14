0f5779939f762fb6f1b62763912cd59bf05e664d
Michael J. Radwin <michael@radwin.org>
2014-02-13 20:40:59 -0800

    more Digital Ocean changes

--- hebcal.com/bin/cache_cleanup.sh
+++ hebcal.com/bin/cache_cleanup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-HEBCAL_WEB=/home/hebcal/web/hebcal.com
+CACHEDIR=/var/www/cache/shabbat
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export PATH
 
-if [ -d $HEBCAL_WEB/cache ]; then
-    mv -f $HEBCAL_WEB/cache $HEBCAL_WEB/cache.$$
-    find $HEBCAL_WEB/cache.$$ -type f -delete
-    rm -rf $HEBCAL_WEB/cache.$$
+if [ -d $CACHEDIR ]; then
+    mv -f $CACHEDIR $CACHEDIR.$$
+    find $HCACHEDIR.$$ -type f -delete
+    rm -rf $CACHEDIR.$$
 fi
