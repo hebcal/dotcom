9d24ad7b99636bc1f7ea1a8a8075f9b509996118
Ubuntu <michael@radwin.org>
2014-10-02 22:04:06 +0000

    Use localhost:8080 for real backend fetch

--- download.hebcal.com/export/err404.php
+++ download.hebcal.com/export/err404.php
@@ -30,9 +30,9 @@ if ($args_pos !== false) {
 	header("Status: 200 OK");
 	header("Content-Type: text/calendar; charset=UTF-8");
 	if ($param["v"] == "1") {
-	    $url = $url_prefix . "/hebcal/index.cgi/export.ics" . $args;
+	    $url = "http://localhost:8080/hebcal/index.cgi/export.ics" . $args;
 	} elseif ($param["v"] == "yahrzeit") {
-	    $url = $url_prefix . "/yahrzeit/yahrzeit.cgi/export.ics" . $args;
+	    $url = "http://localhost:8080/yahrzeit/yahrzeit.cgi/export.ics" . $args;
 	}
 	$ch = curl_init($url);
 	$user_agent = "hebcal-export/20130721";
