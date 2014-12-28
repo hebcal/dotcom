a147794f834bc21eb9cbdd0cbd45e8d9a3f75a32
Michael J. Radwin <Michael_Radwin@intuit.com>
2014-12-28 11:17:38 -0800

    more progres on SEO

--- hebcal.com/bin/create_shabbat_browse.pl
+++ hebcal.com/bin/create_shabbat_browse.pl
@@ -67,33 +67,101 @@ if (! -d $outdir) {
     die "$outdir: $!\n";
 }
 
-my %countries;
-foreach my $continent (sort keys %Hebcal::CONTINENTS) {
-    $countries{$continent} = [];
-}
-
 my $dbh = Hebcal::zipcode_open_db($Hebcal::GEONAME_SQLITE_FILE);
 $dbh->{sqlite_unicode} = 1;
-my $sql = qq{SELECT Continent, Country FROM country ORDER BY Continent, Country};
-my $sth = $dbh->prepare($sql) or die $dbh->errstr;
-$sth->execute() or die $dbh->errstr;
-while (my($continent,$country) = $sth->fetchrow_array) {
-    push(@{$countries{$continent}}, $country);
+
+my $countries = get_countries();
+
+write_index_page($countries);
+
+foreach my $continent (keys %Hebcal::CONTINENTS) {
+    foreach my $iso_country (@{$countries->{$continent}}) {
+        my $iso = $iso_country->[0];
+        my $country = $iso_country->[1];
+        my $anchor = Hebcal::make_anchor($country);
+        write_country_page($iso,$anchor,$country);
+    }
 }
-$sth->finish;
+
 Hebcal::zipcode_close_db($dbh);
 undef($dbh);
+exit(0);
+
+sub get_countries {
+    my %countries;
+    foreach my $continent (keys %Hebcal::CONTINENTS) {
+        $countries{$continent} = [];
+    }
+    my $sql = qq{SELECT Continent, ISO, Country FROM country ORDER BY Continent, Country};
+    my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+    $sth->execute() or die $dbh->errstr;
+    while (my($continent,$iso,$country) = $sth->fetchrow_array) {
+        next if $country eq "";
+        push(@{$countries{$continent}}, [$iso, $country]);
+    }
+    $sth->finish;
+    return \%countries;
+}
+
+sub write_country_page {
+    my($iso,$anchor,$country) = @_;
+
+    INFO($country);
+
+    my $fn = "$outdir/$anchor";
+    open(my $fh, ">$fn.$$") || die "$fn.$$: $!\n";
+
+    my $page_title = "$country Shabbat Candle Lighting Times";
+    print $fh Hebcal::html_header_bootstrap3($page_title,
+        "/shabbat/browse/$anchor", "ignored");
+
+    print $fh <<EOHTML;
+<div class="row">
+<div class="col-sm-12">
+<div class="page-header">
+<h1>$country<small> Shabbat Candle Lighting Times</small></h1>
+</div>
+</div><!-- .col-sm-12 -->
+</div><!-- .row -->
+<div class="row">
+<ul class="bullet-list-inline">
+EOHTML
+    ;
+
+    my $sql = qq{SELECT g.geonameid, g.name, g.asciiname, a.name
+FROM geoname g
+LEFT JOIN admin1 a on g.country||'.'||g.admin1 = a.key
+WHERE g.country = ?
+ORDER BY g.asciiname};
+    my $sth = $dbh->prepare($sql) or die $dbh->errstr;
+    $sth->execute($iso) or die $dbh->errstr;
+    while (my($geonameid,$name,$asciiname,$admin1) = $sth->fetchrow_array) {
+        print $fh qq{<li><a href="/shabbat/?geo=geoname&amp;geonameid=$geonameid">$name</a></li>\n};
+    }
+    $sth->finish;
+
+    print $fh qq{</ul>\n};
+    print $fh qq{</div><!-- .row -->\n};
+
+    print $fh Hebcal::html_footer_bootstrap3(undef, undef);
+
+    close($fh);
+    rename("$fn.$$", $fn) || die "$fn: $!\n";
+}
 
 
-my $fn = "$outdir/index.html";
-INFO($fn);
-open(my $fh, ">$fn.$$") || die "$fn.$$: $!\n";
+sub write_index_page {
+    my($countries) = @_;
 
-my $page_title = "Shabbat Candle Lighting Times";
-print $fh Hebcal::html_header_bootstrap3($page_title,
-    "/shabbat/browse", "ignored");
+    my $fn = "$outdir/index.html";
+    INFO($fn);
+    open(my $fh, ">$fn.$$") || die "$fn.$$: $!\n";
 
-print $fh <<EOHTML;
+    my $page_title = "Shabbat Candle Lighting Times";
+    print $fh Hebcal::html_header_bootstrap3($page_title,
+        "/shabbat/browse/", "ignored");
+
+    print $fh <<EOHTML;
 <div class="row">
 <div class="col-sm-12">
 <div class="page-header">
@@ -103,26 +171,29 @@ print $fh <<EOHTML;
 </div><!-- .row -->
 <div class="row">
 EOHTML
-;
+    ;
 
-foreach my $continent (sort keys %Hebcal::CONTINENTS) {
-    my $name = $Hebcal::CONTINENTS{$continent};
-    print $fh <<EOHTML;
+    foreach my $continent (qw(EU NA SA OC AS AF AN)) {
+        my $name = $Hebcal::CONTINENTS{$continent};
+        print $fh <<EOHTML;
 <div class="col-sm-4">
 <h3>$name</h3>
-<ul>
+<ul class="bullet-list-inline">
 EOHTML
-;
-    foreach my $country (@{$countries{$continent}}) {
-        my $anchor = Hebcal::make_anchor($country);
-        print $fh qq{<li><a href="$anchor">$country</a></li>\n};
+    ;
+        foreach my $iso_country (@{$countries->{$continent}}) {
+            my $iso = $iso_country->[0];
+            my $country = $iso_country->[1];
+            my $anchor = Hebcal::make_anchor($country);
+            print $fh qq{<li><a href="$anchor">$country</a></li>\n};
+        }
+        print $fh "</ul>\n</div><!-- .col-sm-4 -->\n";
     }
-    print $fh "</ul>\n</div><!-- .col-sm-4 -->\n";
+
+    print $fh qq{</div><!-- .row -->\n};
+
+    print $fh Hebcal::html_footer_bootstrap3(undef, undef);
+
+    close($fh);
+    rename("$fn.$$", $fn) || die "$fn: $!\n";
 }
-
-print $fh qq{</div><!-- .row -->\n};
-
-print $fh Hebcal::html_footer_bootstrap3(undef, undef);
-
-close($fh);
-rename("$fn.$$", $fn) || die "$fn: $!\n";
