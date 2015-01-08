3cb8519e53164bf1409e535365d91ef6522a5e27
Michael J. Radwin <Michael_Radwin@intuit.com>
2015-01-07 21:28:52 -0800

    bootstrap3

--- hebcal.com/link/index.php
+++ hebcal.com/link/index.php
@@ -11,38 +11,45 @@ if (isset($param["m"]) && is_numeric($param["m"])) {
 } else {
     $m = 50;
 }
-if (isset($param["zip"]) && preg_match('/^\d{5}$/', $param["zip"])) {
-    $zip = $param["zip"];
-} else {
-    $zip = 90210;
+
+function my_geoname_city_descr($geonameid) {
+    list($name,$asciiname,$country,$admin1,$latitude,$longitude,$tzid) =
+        hebcal_get_geoname($geonameid);
+    return geoname_city_descr($name,$admin1,$country);
 }
 
-if (isset($param["city"])) {
-    $geo = "city";
-    $geo_city = $param["city"];
-    $geo_link = "geo=city&amp;city=" . urlencode($geo_city);
+$geo = $geonameid = $zip = $city = $city_descr = "";
 
-    global $hebcal_cities, $hebcal_countries;
-    $info = $hebcal_cities[$geo_city];
-    $descr = $info[1] . ", " . $hebcal_countries[$info[0]][0];
-} elseif (isset($param["geonameid"])) {
+if (isset($param["geonameid"]) && is_numeric($param["geonameid"])) {
+    $geonameid = $param["geonameid"];
     $geo = "geoname";
-    $geo_link = "geo=geoname&amp;geonameid=" . $param["geonameid"];
-    $descr = $param["city-typeahead"];
-} else {
+    $city_descr = my_geoname_city_descr($geonameid);
+    $geo_link = "geonameid=$geonameid";
+} elseif (isset($param["zip"]) && is_numeric($param["zip"])) {
+    $zip = $param["zip"];
     $geo = "zip";
-    list($city,$state,$tzid,$latitude,$longitude,
-	 $lat_deg,$lat_min,$long_deg,$long_min) =
-	hebcal_get_zipcode_fields($zip);
-
-    if (!$state) {
-	$city = "Unknown";
-	$state = "ZZ";
+    list($city,$state,$tzid,$latitude,$longitude,$lat_deg,$lat_min,$long_deg,$long_min) =
+        hebcal_get_zipcode_fields($zip);
+    $city_descr = "$city, $state $zip";
+    $geo_link = "zip=$zip";
+} elseif (isset($param["city"]) && isset($hebcal_cities[$param["city"]])) {
+    $city = $param["city"];
+    if (isset($hebcal_cities_old[$city])) {
+        $city = $hebcal_cities_old[$city];
     }
+    $geo = "city";
+    $city_info = $hebcal_cities[$city];
+    $city_descr = $city_info[1] . ", " . $hebcal_countries[$city_info[0]][0];
+    $geo_link = "city=" . urlencode($city);
+} else {
+    $geonameid = 281184;
+    $geo = "geoname";
+    $city_descr = my_geoname_city_descr($geonameid);
+    $geo_link = "geonameid=$geonameid";
+}
 
-    $geo_link = "geo=zip&amp;zip=" . urlencode($zip);
-    $descr = htmlspecialchars("$city, $state $zip");
-    $zip = htmlspecialchars($zip);
+if (empty($param["city-typeahead"])) {
+    $param["city-typeahead"] = $city_descr;
 }
 
 if (isset($param["a"]) && ($param["a"] == "1" || $param["a"] == "on")) {
@@ -52,132 +59,104 @@ if (isset($param["a"]) && ($param["a"] == "1" || $param["a"] == "on")) {
     $ashk = "";
 }
 
-$url_base = "//www.hebcal.com/shabbat/?${geo_link}&amp;m=${m}";
-$url_base_double = htmlentities($url_base);
-
 $xtra_head = <<<EOD
 <style type="text/css">
 ul.hebcal-results { list-style-type: none }
+#change-city {
+  margin-top: 30px;
+  margin-bottom: 80px;
+}
 </style>
 <script type="text/javascript" src="/i/sh-3.0.83/scripts/shCore.js"></script>
 <script type="text/javascript" src="/i/sh-3.0.83/scripts/shBrushXml.js"></script>
 <script type="text/javascript" src="/i/sh-3.0.83/scripts/shBrushCss.js"></script>
 <link type="text/css" rel="stylesheet" href="/i/sh-3.0.83/styles/shCoreDefault.css">
-<link rel="stylesheet" type="text/css" href="/i/hebcal-typeahead-v1.1.min.css">
+<link rel="stylesheet" type="text/css" href="/i/hyspace-typeahead.css">
 <script type="text/javascript">SyntaxHighlighter.all();</script>
 EOD;
 
-echo html_header_bootstrap("Add weekly Shabbat candle-lighting times to your synagogue website",
-		     $xtra_head);
+echo html_header_bootstrap3("Add weekly Shabbat candle-lighting times to your synagogue website",
+                     $xtra_head);
 ?>
-<div class="page-header">
+<div class="row">
+<div class="col-sm-12">
 <h1>Add Shabbat Times to your Website</h1>
-</div>
 <p class="lead">Use these HTML tags to add weekly Shabbat candle-lighting
 times and Torah portion directly on your synagogue's website.</p>
 <p>Browse the <a href="/home/category/developers">Hebcal web developer
 APIs</a> to display other information on your site (e.g. today's
 Hebrew date, a full Jewish Calendar, RSS feeds).</p>
 <p>The following tags are for
-<b><?php echo $descr ?></b>
+<b><?php echo $city_descr ?></b>
 (<a href="#change">change city</a>).</p>
 <p><b>Instructions:</b> Copy everything from this box and paste it into
 the appropriate place in your HTML:</p>
-<pre class="brush:html;auto-links:false"">
-&lt;script type="text/javascript" charset="utf-8"
-src="<?php echo $url_base_double ?>&amp;amp;cfg=j&amp;amp;tgt=_top"&gt;
-&lt;/script&gt;
+<?php
+$script_tag = <<<EOHTML
+<script type="text/javascript" charset="utf-8"
+src="//www.hebcal.com/shabbat/?geo=${geo}&amp;${geo_link}&amp;m=${m}&amp;cfg=j&amp;tgt=_top">
+</script>
+EOHTML;
+$script_tag_double = htmlentities($script_tag);
+?>
+<pre class="brush:html;auto-links:false">
+<?php echo $script_tag_double ?>
 </pre>
 
 <p>The result will look like this (<a href="#fonts">customize fonts</a>):</p>
 
 <div class="box">
-<script type="text/javascript" charset="utf-8"
-src="<?php echo $url_base ?>&amp;cfg=j&amp;tgt=_top">
-</script>
+<?php echo $script_tag ?>
 </div><!-- .box -->
 
+<div id="change-city">
 <h2 id="change">Change City</h2>
 
 <p>Enter a new city to get revised HTML tags for your
 synagogue's web page.</p>
 
-<div class="row-fluid">
-<div class="tabbable">
-  <ul class="nav nav-tabs">
-    <li<?php if ($geo == "zip") { echo ' class="active"';} ?>><a href="#tab-zip" data-toggle="tab">ZIP code</a></li>
-    <li<?php if ($geo == "city") { echo ' class="active"';} ?>><a href="#tab-city" data-toggle="tab">Major City</a></li>
-    <li<?php if ($geo == "geoname") { echo ' class="active"';} ?>><a href="#tab-search" data-toggle="tab">Search</a></li>
-  </ul>
-  <div class="tab-content">
-    <div class="tab-pane<?php if ($geo == "zip") { echo ' active';} ?>" id="tab-zip">
 <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="get">
-<fieldset>
-<input type="hidden" name="geo" value="zip">
-<label>ZIP code:
-<input type="text" name="zip" value="<?php echo $zip ?>" size="5" maxlength="5" style="width:auto"></label>
-<label>Havdalah minutes past sundown:
-<input type="text" name="m" value="<?php echo $m ?>" size="3" maxlength="3" style="width:auto"></label>
-<small class="help-block">(enter "0" to turn off Havdalah times)</small>
-<label class="checkbox"><input type="checkbox" name="a"<?php echo $ashk ?>>
-Use Ashkenazis Hebrew transliterations</label>
-<input type="submit" class="btn btn-primary" value="Get new HTML tags">
-</fieldset></form>
-    </div><!-- #tab-zip -->
-    <div class="tab-pane<?php if ($geo == "city") { echo ' active';} ?>" id="tab-city">
-<form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="get">
-<fieldset>
-<input type="hidden" name="geo" value="city">
-<?php
-echo html_city_select(isset($geo_city) ? $geo_city : "IL-Jerusalem");
-?>
-<label>Havdalah minutes past sundown:
-<input type="text" name="m" value="<?php echo $m ?>" size="3" maxlength="3" style="width:auto">
-</label>
-<small class="help-block">(enter "0" to turn off Havdalah times)</small>
-<label class="checkbox"><input type="checkbox" name="a"<?php echo $ashk ?>>
-Use Ashkenazis Hebrew transliterations</label>
-<input type="submit" class="btn btn-primary" value="Get new HTML tags">
-</fieldset></form>
-    </div><!-- #tab-city -->
-    <div class="tab-pane<?php if ($geo == "geoname") { echo ' active';} ?>" id="tab-search">
-<form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="get">
-<fieldset>
 <input type="hidden" name="geo" id="geo" value="geoname">
 <input type="hidden" name="zip" id="zip" value="">
-<input type="hidden" name="geonameid" id="geonameid" value="">
-<div class="city-typeahead form-inline" style="margin-bottom:12px">
-<input type="text" name="city-typeahead" id="city-typeahead" class="form-control input-xlarge" placeholder="Search for city" value="<?php
-  echo isset($param["city-typeahead"]) ? htmlentities($param["city-typeahead"]) : "" ?>">
+<input type="hidden" name="city" id="city" value="">
+<input type="hidden" name="geonameid" id="geonameid" value="<?php echo htmlspecialchars($param["geonameid"]) ?>">
+
+<div class="form-group">
+  <label for="city-typeahead">City</label>
+  <div class="city-typeahead" style="margin-bottom:12px">
+    <input type="text" name="city-typeahead" id="city-typeahead" class="form-control" placeholder="Search for city" value="<?php echo htmlentities($param["city-typeahead"]) ?>">
+  </div>
 </div>
-<label>Havdalah minutes past sundown:
-<input type="text" name="m" value="<?php echo $m ?>" size="3" maxlength="3" style="width:auto">
-</label>
-<small class="help-block">(enter "0" to turn off Havdalah times)</small>
-<label class="checkbox"><input type="checkbox" name="a"<?php echo $ashk ?>>
-Use Ashkenazis Hebrew transliterations</label>
+
+<div class="form-group">
+  <label for="m">Havdalah minutes past sundown
+    <a href="#" id="havdalahInfo" data-toggle="tooltip" data-placement="top" title="Use 42 min for three medium-sized stars, 50 min for three small stars, 72 min for Rabbeinu Tam, or 0 to suppress Havdalah times"><span class="glyphicon glyphicon-info-sign"></span></a>
+  </label>
+  <input type="text" name="m" id="m" class="form-control" pattern="\d*" value="<?php echo $m ?>" maxlength="3">
+</div>
+<div class="checkbox">
+  <label>
+    <input type="checkbox" name="a"<?php echo $ashk ?>>
+    Use Ashkenazis Hebrew transliterations
+  </label>
+</div>
 <input type="submit" class="btn btn-primary" value="Get new HTML tags">
 </fieldset></form>
-    </div><!-- #tab-search -->
-  </div><!-- .tab-content -->
-</div><!-- .tabbable -->
-
-</div><!-- .row-fluid -->
+</div><!-- #change-city -->
 
 <h2 id="fonts">Customize Fonts</h2>
 
 <p>To change the fonts to match the rest of your site, you can add a
-CSS stylesheet like this to the
+&lt;style type="text/css"&gt; stylesheet like this to the
 <tt>&lt;head&gt; ... &lt;/head&gt;</tt> section at the top of your web
 page:</p>
 
-<pre class="brush:html">
-&lt;style type="text/css"&gt;
+<pre class="brush:css">
 #hebcal {
  font-family: "Gill Sans MT","Gill Sans",GillSans,Arial,Helvetica,sans-serif;
  font-size: small;
 }
-#hebcal H3 {
+#hebcal h3 {
  font-family: Georgia,Palatino,"Times New Roman",Times,serif;
 }
 ul.hebcal-results { list-style-type:none }
@@ -191,45 +170,24 @@ ul.hebcal-results li {
 .hebcal-results .havdalah { color: green }
 .hebcal-results .parashat { color: black; background: #ff9 }
 .hebcal-results .holiday { display: none }
-&lt;/style&gt;
 </pre>
 
 <p>Those fonts and colors are just an example.  <a
 href="http://www.w3.org/Style/CSS/">Cascading Style Sheets (CSS)</a> are
 very powerful and flexible.</p>
-
+</div><!-- .col-sm-12 -->
+</div><!-- .row -->
 <?php
 $xtra_html = <<<EOD
-<script src="/i/typeahead-0.9.3.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/typeahead.js/0.10.4/typeahead.bundle.min.js"></script>
+<script src="/i/hebcal-app-1.2.min.js"></script>
 <script type="text/javascript">
-$("#city-typeahead").typeahead({
-    name: "hebcal-city",
-    remote: "/complete.php?q=%QUERY",
-    limit: 5,
-    template: function(ctx) {
-      if (typeof ctx.geo === "string" && ctx.geo == "zip") {
-        return '<p>' + ctx.asciiname + ', ' + ctx.admin1 + ' <strong>' + ctx.id + '</strong> - United States</p>';
-      } else {
-        return '<p><strong>' + ctx.asciiname + '</strong> - <small>' + ctx.admin1 + ', ' + ctx.country + '</small></p>';
-      }
-    }
-}).on('typeahead:selected', function (obj, datum, name) {
-  if (typeof datum.geo === "string" && datum.geo == "zip") {
-    $('#geo').val('zip');
-    $('#zip').val(datum.id);
-  } else {
-    $('#geo').val('geoname');
-    $('#geonameid').val(datum.id);
-  }
-}).bind("keyup keypress", function(e) {
-  var code = e.keyCode || e.which;
-  if (code == 13) {
-    e.preventDefault();
-    return false;
-  }
-});
+window['hebcal'].createCityTypeahead(false);
+$('#havdalahInfo').click(function(e){
+ e.preventDefault();
+}).tooltip();
 </script>
 EOD;
-    echo html_footer_bootstrap(true, $xtra_html);
+    echo html_footer_bootstrap3(true, $xtra_html);
     exit();
 ?>
