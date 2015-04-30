ab56b990107c7b1be527312abc7d81c9bb701684
Michael J. Radwin <Michael_Radwin@intuit.com>
2015-04-29 20:02:56 -0700

    Add many more Israeli towns

--- local/bin/geonames_cities_sqlite.pl
+++ local/bin/geonames_cities_sqlite.pl
@@ -4,38 +4,39 @@ use strict;
 use DBI;
 use Carp;
 
-die "usage: $0 geonames.sqlite3 countryInfo.txt cities15000.txt admin1CodesASCII.tx \n" unless @ARGV == 4;
+die "usage: $0 geonames.sqlite3 countryInfo.txt cities15000.txt admin1CodesASCII.txt IL.txt\n" unless @ARGV == 5;
 
 my $file = shift;
 my $in_country = shift;
 my $in_cities = shift;
 my $in_admin1 = shift;
+my $in_israel = shift;
 
 my $dbh = DBI->connect("dbi:SQLite:dbname=$file", "", "",
-		       { RaiseError => 1, AutoCommit => 0 })
+                       { RaiseError => 1, AutoCommit => 0 })
     or die $DBI::errstr;
 
 do_sql($dbh, qq{DROP TABLE IF EXISTS geoname});
 
-do_sql($dbh, qq{CREATE TABLE geoname ( 
-    geonameid int PRIMARY KEY, 
-    name nvarchar(200), 
-    asciiname nvarchar(200), 
-    alternatenames nvarchar(4000), 
-    latitude decimal(18,15), 
-    longitude decimal(18,15), 
-    fclass nchar(1), 
-    fcode nvarchar(10), 
-    country nvarchar(2), 
-    cc2 nvarchar(60), 
-    admin1 nvarchar(20), 
-    admin2 nvarchar(80), 
-    admin3 nvarchar(20), 
-    admin4 nvarchar(20), 
-    population int, 
-    elevation int, 
-    gtopo30 int, 
-    timezone nvarchar(40), 
+do_sql($dbh, qq{CREATE TABLE geoname (
+    geonameid int PRIMARY KEY,
+    name nvarchar(200),
+    asciiname nvarchar(200),
+    alternatenames nvarchar(4000),
+    latitude decimal(18,15),
+    longitude decimal(18,15),
+    fclass nchar(1),
+    fcode nvarchar(10),
+    country nvarchar(2),
+    cc2 nvarchar(60),
+    admin1 nvarchar(20),
+    admin2 nvarchar(80),
+    admin3 nvarchar(20),
+    admin4 nvarchar(20),
+    population int,
+    elevation int,
+    gtopo30 int,
+    timezone nvarchar(40),
     moddate date);});
 
 do_sql($dbh, qq{DROP TABLE IF EXISTS admin1});
@@ -74,6 +75,7 @@ do_sql($dbh, qq{CREATE TABLE country (
 do_file($dbh,$in_country,"country",19);
 do_file($dbh,$in_cities,"geoname",19);
 do_file($dbh,$in_admin1,"admin1",4);
+do_file($dbh,$in_israel,"geoname",19,\&filter_places);
 
 do_sql($dbh, qq{DROP TABLE IF EXISTS geoname_fulltext});
 
@@ -116,12 +118,17 @@ sub do_sql {
     $dbh->commit;
 }
 
-sub do_file {
-    my($dbh,$infile,$table_name,$expected_fields) = @_;
+sub filter_places {
+    my($a) = @_;
+    return $a->[6] eq "P" && $a->[7] =~ /^(PPL|STLMT)$/; #  && $a->[14] ne "0";
+}
 
-    my $sql = "INSERT INTO $table_name VALUES (?";
+sub do_file {
+    my($dbh,$infile,$table_name,$expected_fields,$callback) = @_;
+
+    my $sql = "INSERT OR IGNORE INTO $table_name VALUES (?";
     for (my $i = 0; $i < $expected_fields - 1; $i++) {
-	$sql .= ",?";
+        $sql .= ",?";
     }
     $sql .= ")";
     print STDERR $sql, "\n";
@@ -129,22 +136,26 @@ sub do_file {
 
     my $fh;
     open($fh, "<:encoding(UTF-8)", $infile)
-	or die "cannot open < $infile: $!";
+        or die "cannot open < $infile: $!";
 
     my $i = 0;
     while(<$fh>) {
-	chomp;
-	next if /^#/;
-	my @a = split(/\t/, $_, -1);
-	my $actual = scalar(@a);
-	if ($actual != $expected_fields) {
-	    carp "$infile:$.: got $actual fields (expected $expected_fields)\n";
-	    next;
-	}
-	my $rv = $sth->execute(@a) or die $dbh->errstr;
-	if (0 == $i++ % 1000) {
-	    $dbh->commit;
-	}
+        chomp;
+        next if /^#/;
+        my @a = split(/\t/, $_, -1);
+        my $actual = scalar(@a);
+        if ($actual != $expected_fields) {
+            carp "$infile:$.: got $actual fields (expected $expected_fields)\n";
+            next;
+        }
+        if ($callback) {
+            my $accept = &$callback(\@a);
+            next unless $accept;
+        }
+        my $rv = $sth->execute(@a) or die $dbh->errstr;
+        if (0 == $i++ % 1000) {
+            $dbh->commit;
+        }
     }
     close($fh);
     $sth->finish;
