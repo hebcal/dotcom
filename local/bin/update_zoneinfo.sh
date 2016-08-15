d91367cda1c2bad1dc25f62712ce1487a2b68e74
Michael J. Radwin <Michael_Radwin@intuit.com>
2016-08-15 13:50:18 -0700

    update zoneinfo script

--- local/bin/update_zoneinfo.sh
+++ local/bin/update_zoneinfo.sh
@@ -2,6 +2,13 @@
 
 set -x
 
-nice wget --mirror --no-parent --limit-rate=40k http://tzurl.org/zoneinfo/
+MYTMPDIR=`mktemp -d /tmp/tmp.XXXXXXXXXX`
+
+cd $MYTMPDIR
+
+nice wget --quiet --mirror --no-parent --limit-rate=40k http://tzurl.org/zoneinfo/
 find tzurl.org/zoneinfo/ -name 'index.html*' -delete
-rsync -av tzurl.org/zoneinfo/ /var/www/zoneinfo/
+rsync -av tzurl.org/zoneinfo/ /var/www/html/zoneinfo/
+
+cd /
+rm -rf $MYTMPDIR
